4927e30d3cb7d13321da70c55131be1417d4771c
qingwei.li <cinwell.li@gmail.com>
2016-08-16 16:11:43 +0800

    remove unused code

--- scripts/cooking.demo.js
+++ scripts/cooking.demo.js
@@ -86,6 +86,4 @@ cooking.add('externals', externals);
 
 // cooking.config.vue.loaders.html = 'html?minimize=true&conservativeCollapse=false';
 
-cooking.add('vue.loaders')
-
 module.exports = cooking.resolve();
