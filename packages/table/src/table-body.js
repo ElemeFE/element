6eaa9ef545331fc573ddf8bf7ef717f803d41ddd
qingwei.li <cinwell.li@gmail.com>
2016-08-25 13:43:09 +0800

    Table: get context in custom template

--- packages/table/src/table-body.js
+++ packages/table/src/table-body.js
@@ -59,7 +59,7 @@ export default {
                       on-mouseleave={ this.handleCellMouseLeave }>
                       {
                         column.template
-                          ? column.template.call(this._renderProxy, h, { row, column, $index })
+                          ? column.template.call(this._renderProxy, h, { row, column, $index, _self: this.$parent.$parent })
                           : <div class="cell">{ this.$getPropertyText(row, column.property, column.id) }</div>
                       }
                     </td>
