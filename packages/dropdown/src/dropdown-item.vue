b5a5cee0037cdf1d726c82748d725b151eaaa534
baiyaaaaa <bigpet1991@gmail.com>
2016-10-13 13:54:02 +0800

    fix dropdown menu click

--- packages/dropdown/src/dropdown-item.vue
+++ packages/dropdown/src/dropdown-item.vue
@@ -1,8 +1,18 @@
 <template>
-  <li class="el-dropdown-item"><slot></slot></li>
+  <li class="el-dropdown-item" @click="handleClick"><slot></slot></li>
 </template>
 <script>
+  import emitter from 'element-ui/src/mixins/emitter';
+
   export default {
-    name: 'ElDropdownItem'
+    name: 'ElDropdownItem',
+
+    mixins: [emitter],
+
+    methods: {
+      handleClick(e) {
+        this.dispatch('ElDropdownMenu', 'visible', [false]);
+      }
+    }
   };
 </script>
