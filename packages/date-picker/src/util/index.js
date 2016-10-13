8a8c28fc4f03b5d6f24f7c148b4b4c06ec01615c
qingwei.li <cinwell.li@gmail.com>
2016-10-13 16:11:06 +0800

    DatePicker: fix tab key

--- packages/date-picker/src/util/index.js
+++ packages/date-picker/src/util/index.js
@@ -8,22 +8,6 @@ const newArray = function(start, end) {
   return result;
 };
 
-export const merge = function(target) {
-  for (var i = 1, j = arguments.length; i < j; i++) {
-    var source = arguments[i];
-    for (var prop in source) {
-      if (source.hasOwnProperty(prop)) {
-        var value = source[prop];
-        if (value !== undefined) {
-          target[prop] = value;
-        }
-      }
-    }
-  }
-
-  return target;
-};
-
 export const toDate = function(date) {
   date = new Date(date);
   if (isNaN(date.getTime())) return null;
