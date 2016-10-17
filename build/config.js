0a2ec8efb085781de136c0b5a5e29346f3099db5
qingwei.li <cinwell.li@gmail.com>
2016-10-17 17:47:24 +0800

    Fix build common config

--- build/config.js
+++ build/config.js
@@ -1,5 +1,6 @@
 var Components = require('../components.json');
 var path = require('path');
+var dependencies = require('../package.json').dependencies;
 var externals = {};
 
 Object.keys(Components).forEach(function(key) {
@@ -7,18 +8,16 @@ Object.keys(Components).forEach(function(key) {
   externals[`element-ui/packages/${key}/style.css`] = `element-ui/lib/${key}/style.css`;
 });
 
+Object.keys(dependencies).forEach(function (key) {
+  externals[key] = key;
+});
+
 externals['element-ui/src/utils/clickoutside'] = 'element-ui/lib/utils/clickoutside';
 externals['element-ui/src/utils/popper'] = 'element-ui/lib/utils/popper';
 externals['element-ui/src/utils/vue-popper'] = 'element-ui/lib/utils/vue-popper';
-externals['vue-popup'] = 'vue-popup';
 
 exports.externals = Object.assign({
-  vue: {
-    root: 'Vue',
-    commonjs: 'vue',
-    commonjs2: 'vue',
-    amd: 'vue'
-  }
+  vue: 'vue'
 }, externals);
 
 exports.alias = {
@@ -26,6 +25,13 @@ exports.alias = {
   packages: path.resolve(__dirname, '../packages'),
   examples: path.resolve(__dirname, '../examples'),
   'element-ui': path.resolve(__dirname, '../')
+};
+
+exports.vue = {
+  root: 'Vue',
+  commonjs: 'vue',
+  commonjs2: 'vue',
+  amd: 'vue'
 };
 
 exports.jsexclude = /node_modules|utils\/popper\.js|utils\/date.\js/;
