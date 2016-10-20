6d0673e69516857f50ba6539ea1299f9d9bf8879
qingwei.li <cinwell.li@gmail.com>
2016-10-20 20:37:59 +0800

    Upload: fix links in the doc

--- build/deploy-ci.sh
+++ build/deploy-ci.sh
@@ -5,8 +5,8 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$GH_TOKEN" ]; then
   mkdir temp_web
   cd temp_web
   git clone https://$GH_TOKEN@github.com/ElementUI/dev.git && cd dev
-  git config user.name "$(git log --no-merges -n 1 --format=%an)"
-  git config user.email "$(git log --no-merges -n 1 --format=%ae)"
+  git config user.name "$TRAVIS_COMMIT_USER"
+  git config user.email "$TRAVIS_COMMIT_EMAIL"
   rm -rf *
   cp -rf ../../examples/element-ui/** .
   git add -A .
