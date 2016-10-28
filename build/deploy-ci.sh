2453d63654138c47abcf96a4304bf5d082984c68
qingwei.li <cinwell.li@gmail.com>
2016-10-28 18:09:07 +0800

    travis: fix shell

--- build/deploy-ci.sh
+++ build/deploy-ci.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
-mkdir temp_web && cd temp_web
+mkdir temp_web
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$GH_TOKEN" ]; then
   CI_ENV=/dev/ ./node_modules/.bin/cooking build -c build/cooking.demo.js
+  cd temp_web
   git clone https://$GH_TOKEN@github.com/ElementUI/dev.git && cd dev
   git config user.name "$TRAVIS_COMMIT_USER"
   git config user.email "$TRAVIS_COMMIT_EMAIL"
@@ -11,11 +12,12 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$GH_TOKEN" ]; then
   git add -A .
   git commit -m "$TRAVIS_COMMIT_MSG"
   git push origin master
-  cd ..
+  cd ../..
 fi
 
 if [ "$TRAVIS_TAG" ] && [ "$GH_TOKEN" ]; then
   npm run dist
+  cd temp_web
   git clone https://$GH_TOKEN@github.com/ElementUI/lib.git && cd lib
   git config user.name "$TRAVIS_COMMIT_USER"
   git config user.email "$TRAVIS_COMMIT_EMAIL"
@@ -25,5 +27,5 @@ if [ "$TRAVIS_TAG" ] && [ "$GH_TOKEN" ]; then
   git commit -m "[build] $TRAVIS_TAG"
   git tag $TRAVIS_TAG
   git push origin master --tags
-  cd ..
+  cd ../..
 fi
