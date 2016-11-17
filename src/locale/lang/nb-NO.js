0e35ab2e62770730e0cab4ae1a93e6a5661d2243
Ole Magnus Støvern <olestovern@gmail.com>
2016-11-18 00:51:20 +0100

    Added Norwegian bokmål (#1164)
    Norway got two different written languages, this is the most common one.

---
+++ src/locale/lang/nb-NO.js
@@ -0,0 +1,85 @@
+export default {
+  el: {
+    datepicker: {
+      now: 'Nå',
+      today: 'I dag',
+      cancel: 'Avbryt',
+      clear: 'Tøm',
+      confirm: 'OK',
+      selectDate: 'Velg dato',
+      selectTime: 'Velg tidspunkt',
+      startDate: 'Start Dato',
+      startTime: 'Start Tidspunkt',
+      endDate: 'Sluttdato',
+      endTime: 'Sluttidspunkt',
+      year: '',
+      month1: 'Januar',
+      month2: 'Februar',
+      month3: 'Mars',
+      month4: 'April',
+      month5: 'Mai',
+      month6: 'Juni',
+      month7: 'Juli',
+      month8: 'August',
+      month9: 'September',
+      month10: 'Oktober',
+      month11: 'November',
+      month12: 'Desember',
+      // week: 'week',
+      weeks: {
+        sun: 'Søn',
+        mon: 'Man',
+        tue: 'Tir',
+        wed: 'Ons',
+        thu: 'Tor',
+        fri: 'Fre',
+        sat: 'Lør'
+      },
+      months: {
+        jan: 'Jan',
+        feb: 'Feb',
+        mar: 'Mar',
+        apr: 'Apr',
+        may: 'Mai',
+        jun: 'Jun',
+        jul: 'Jul',
+        aug: 'Aug',
+        sep: 'Sep',
+        oct: 'Okt',
+        nov: 'Nov',
+        dec: 'Des'
+      }
+    },
+    select: {
+      loading: 'Laster',
+      noMatch: 'Ingen samsvarende data',
+      noData: 'Ingen data',
+      placeholder: 'Velg'
+    },
+    pagination: {
+      goto: 'Gå til',
+      pagesize: '/side',
+      total: 'Total {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      confirm: 'OK',
+      cancel: 'Avbryt',
+      error: 'Ugyldig input'
+    },
+    upload: {
+      delete: 'Slett',
+      preview: 'Forhåndsvisning',
+      continue: 'Fortsett'
+    },
+    table: {
+      emptyText: 'Ingen Data',
+      confirmFilter: 'Bekreft',
+      resetFilter: 'Tilbakestill',
+      clearFilter: 'Alle'
+    },
+    tree: {
+      emptyText: 'Ingen Data'
+    }
+  }
+};
