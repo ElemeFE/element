bc04f949a38e1a16c7221ba54d971f80d8b634bb
YanagiEiichi <admin@web-tinker.com>
2016-11-18 13:56:30 +0800

    Add ja.js

---
+++ src/locale/lang/ja.js
@@ -0,0 +1,86 @@
+export default {
+  el: {
+    datepicker: {
+      now: '今',
+      today: '今日',
+      cancel: 'キャンセルする',
+      clear: 'クリヤー',
+      confirm: 'はい',
+      selectDate: '日付を入力',
+      selectTime: '時間を入力',
+      startDate: '開始日',
+      startTime: '開始時間',
+      endDate: '終了日',
+      endTime: '終了時間',
+      year: '年',
+      month1: '一月',
+      month2: '二月',
+      month3: '三月',
+      month4: '四月',
+      month5: '五月',
+      month6: '六月',
+      month7: '七月',
+      month8: '八月',
+      month9: '九月',
+      month10: '十月',
+      month11: '十一月',
+      month12: '十二月',
+      // week: 'week',
+      weeks: {
+        sun: '日',
+        mon: '月',
+        tue: '火',
+        wed: '水',
+        thu: '木',
+        fri: '金',
+        sat: '土'
+      },
+      months: {
+        jan: '一月',
+        feb: '二月',
+        mar: '三月',
+        apr: '四月',
+        may: '五月',
+        jun: '六月',
+        jul: '七月',
+        aug: '八月',
+        sep: '九月',
+        oct: '十月',
+        nov: '十一月',
+        dec: '十二月'
+      }
+    },
+    select: {
+      loading: 'ロード中',
+      noMatch: 'データなし',
+      noData: 'データなし',
+      placeholder: 'セレクター'
+    },
+    pagination: {
+      goto: '',
+      pagesize: '件/頁',
+      total: '総計 {total} 件',
+      pageClassifier: 'に跳ぶ'
+    },
+    messagebox: {
+      title: 'メッセージ',
+      confirm: 'はい',
+      cancel: 'キャンセルする',
+      error: 'エラー'
+    },
+    upload: {
+      delete: '削除する',
+      preview: 'プレビュー',
+      continue: '次の件をアップロードする'
+    },
+    table: {
+      emptyText: 'データなし',
+      confirmFilter: 'フィルターする',
+      resetFilter: 'リセットする',
+      clearFilter: 'すべて'
+    },
+    tree: {
+      emptyText: 'データなし'
+    }
+  }
+};
