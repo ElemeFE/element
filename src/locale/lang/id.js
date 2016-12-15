12d43adeb36175293d25e6f5d342b57b9e642e42
Rizqy H <rizqy22@gmail.com>
2016-12-15 11:34:42 +0000

    Add Indonesian translation

---
+++ src/locale/lang/id.js
@@ -0,0 +1,86 @@
+export default {
+  el: {
+    datepicker: {
+      now: 'Sekarang',
+      today: 'Hari ini',
+      cancel: 'Batal',
+      clear: 'Kosongkan',
+      confirm: 'YA',
+      selectDate: 'Pilih tanggal',
+      selectTime: 'Pilih waktu',
+      startDate: 'Tanggal Mulai',
+      startTime: 'Waktu Mulai',
+      endDate: 'Tanggal Selesai',
+      endTime: 'Waktu Selesai',
+      year: 'Tahun',
+      month1: 'Januari',
+      month2: 'Februari',
+      month3: 'Maret',
+      month4: 'April',
+      month5: 'Mei',
+      month6: 'Juni',
+      month7: 'Juli',
+      month8: 'Agustus',
+      month9: 'September',
+      month10: 'Oktober',
+      month11: 'November',
+      month12: 'Desember',
+      // week: 'minggu',
+      weeks: {
+        sun: 'Min',
+        mon: 'Sen',
+        tue: 'Sel',
+        wed: 'Rab',
+        thu: 'Kam',
+        fri: 'Jum',
+        sat: 'Sab'
+      },
+      months: {
+        jan: 'Jan',
+        feb: 'Feb',
+        mar: 'Mar',
+        apr: 'Apr',
+        may: 'Mei',
+        jun: 'Jun',
+        jul: 'Jul',
+        aug: 'Agu',
+        sep: 'Sep',
+        oct: 'Okt',
+        nov: 'Nov',
+        dec: 'Des'
+      }
+    },
+    select: {
+      loading: 'Memuat',
+      noMatch: 'Tidak ada data yang cocok',
+      noData: 'Tidak ada data',
+      placeholder: 'Pilih'
+    },
+    pagination: {
+      goto: 'Pergi ke',
+      pagesize: '/page',
+      total: 'Total {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'Pesan',
+      confirm: 'YA',
+      cancel: 'Batal',
+      error: 'Masukan ilegal'
+    },
+    upload: {
+      delete: 'Hapus',
+      preview: 'Pratinjau',
+      continue: 'Lanjutkan'
+    },
+    table: {
+      emptyText: 'Tidak Ada Data',
+      confirmFilter: 'Konfirmasi',
+      resetFilter: 'Atur Ulang',
+      clearFilter: 'Semua'
+    },
+    tree: {
+      emptyText: 'Tidak Ada Data'
+    }
+  }
+};
