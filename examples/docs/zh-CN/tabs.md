9efd357fdd7d2bfc2cbd5e96658eee11c1afbea6
baiyaaaaa <bigpet1991@gmail.com>
2016-12-22 00:02:51 +0800

    fix tabs initial with bug & improve docs

--- examples/docs/zh-CN/tabs.md
+++ examples/docs/zh-CN/tabs.md
@@ -2,7 +2,18 @@
   export default {
     data() {
       return {
-        activeName: 'first'
+        activeName: 'first',
+        activeName2: 'first',
+        tabs: [{
+          title: 'Tab 1',
+          name: '1',
+          content: 'Tab 1 content'
+        }, {
+          title: 'Tab 2',
+          name: '2',
+          content: 'Tab 2 content'
+        }],
+        tabIndex: 2
       }
     },
     methods: {
@@ -18,18 +29,20 @@
     }
   }
 </script>
+
 ## Tabs 标签页
+
 分隔内容上有关联但属于不同类别的数据集合。
 
 ### 基础用法
 
 基础的、简洁的标签页。
 
-:::demo Tabs 组件提供了选项卡功能，只需要使用`el-tabs`和子元素`el-tab-pane`即可，在两个元素中，我们分别提供了一系列的属性来方便使用，`el-tab-pane`中`label`决定了选项卡标题，标签内部写入内容即可。在下例中我们在`el-tabs`中设置了`active-name`属性，接受一个`String`值，表明选中的选项卡，在`el-tab-pane`中可以设置对应的`name`属性，如果没有设置`name`，则默认值为顺序的`1`/`2`/`3`/`4`。例子选中选项卡2，如果不设置`name`，将`active-name`设为`2`，可以达成相同效果。
+:::demo Tabs 组件提供了选项卡功能，默认选中第一个标签页，你也可以通过 `value` 属性来指定当前选中的标签页。
 
 ```html
 <template>
-  <el-tabs :active-name="activeName">
+  <el-tabs v-model="activeName" @tab-click="handleClick">
     <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
     <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
     <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
@@ -42,6 +55,11 @@
       return {
         activeName: 'first'
       };
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
     }
   };
 </script>
@@ -52,23 +70,25 @@
 
 选项卡样式的标签页。
 
-:::demo 只需要设置`type`属性即可，如果需要标签风格，将其设置为`card`。
+:::demo 只需要设置 `type` 属性为 `card` 就可以使选项卡改变为标签风格。
 
 ```html
 <template>
-  <el-tabs type="card" @tab-click="handleClick" @tab-remove="handleRemove">
-    <el-tab-pane label="用户管理">用户管理</el-tab-pane>
-    <el-tab-pane label="配置管理">配置管理</el-tab-pane>
-    <el-tab-pane label="角色管理">角色管理</el-tab-pane>
-    <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+  <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
+    <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+    <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+    <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+    <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
   </el-tabs>
 </template>
 <script>
   export default {
+    data() {
+      return {
+        activeName: 'first'
+      };
+    },
     methods: {
-      handleRemove(tab) {
-        console.log(tab);
-      },
       handleClick(tab, event) {
         console.log(tab, event);
       }
@@ -82,11 +102,11 @@
 
 可以关闭标签页。
 
-:::demo 在`el-tabs`中设置`closable`属性，接受一个`Boolean`，设置为`true`时为可关闭。
+:::demo 通过设置 `closable` 属性来打开 `Tabs` 的可关闭标签效果, `closable` 也可以设置在 `Tab Panel` 中实现部分标签页的可关闭效果。
 
 ```html
 <template>
-  <el-tabs type="card" :closable="true" @tab-click="handleClick" @tab-remove="handleRemove">
+  <el-tabs type="card" closable @tab-click="handleClick" @tab-remove="handleRemove">
     <el-tab-pane label="用户管理">用户管理</el-tab-pane>
     <el-tab-pane label="配置管理">配置管理</el-tab-pane>
     <el-tab-pane label="角色管理">角色管理</el-tab-pane>
@@ -147,12 +167,28 @@
 ```
 :::
 
+### 动态增加标签页
+
+展示如何通过触发器来动态增加标签页
+
+:::demo
+```html
+<div style="margin-bottom: 20px;">
+  <el-button size="small" @click="tabs.push({ name: 'Tab ' + ++tabIndex, title: 'new Tab', content: 'new Tab content' })">add tab</el-button>
+</div>
+<el-tabs type="card" closable>
+  <el-tab-pane v-for="(item, index) in tabs" :label="item.title" :name="item.name">{{item.content}}</el-tab-pane>
+</el-tabs>
+```
+:::
+
 ### Tabs Attributes
 | 参数       | 说明     | 类型      | 可选值       | 默认值   |
 |---------- |-------- |---------- |-------------  |-------- |
 | type     | 风格类型   | string   | card/border-card  |     —    |
 | closable  | 标签是否可关闭   | boolean   | — |  false  |
-| active-name  | 选中选项卡的 name  | string   |  —  |  第一个选项卡的 name |
+| active-name(deprecated)  | 选中选项卡的 name  | string   |  —  |  第一个选项卡的 name |
+| value  | 绑定值，选中选项卡的 name  | string   |  —  |  第一个选项卡的 name |
 
 ### Tabs Events
 | 事件名称 | 说明 | 回调参数 |
@@ -167,3 +203,4 @@
 | label-content | 选项卡的标题的渲染 Function | Function(h, tab:vueInstance) | - | - |
 | disabled | 是否禁用 | boolean | - | false |
 | name      | 与选项卡 activeName 对应的标识符，表示选项卡别名 | string | — | 该选项卡在选项卡列表中的顺序值，如第一个选项卡则为'1' |
+| closable  | 标签是否可关闭   | boolean   | — |  false  |
--- packages/tabs/src/tabs.vue
+++ packages/tabs/src/tabs.vue
@@ -4,69 +4,84 @@
 
     props: {
       type: String,
-      tabPosition: String,
       activeName: String,
-      closable: false,
-      tabWidth: 0
+      closable: {
+        type: Boolean,
+        default: false
+      },
+      value: {}
     },
 
     data() {
       return {
         children: null,
-        activeTab: null,
-        currentName: 0,
-        panes: []
+        currentName: this.value || this.activeName
       };
     },
 
     watch: {
-      activeName: {
-        handler(val) {
-          this.currentName = val;
-        }
+      activeName(val) {
+        this.currentName = val;
+      },
+      value(val) {
+        this.currentName = val;
+      },
+      currentName(val) {
+        this.$emit('input', val);
       }
     },
 
     methods: {
       handleTabRemove(tab, event) {
         event.stopPropagation();
-        let tabs = this.$children;
+        const tabs = this.$children;
 
-        var index = tabs.indexOf(tab);
-        tab.$destroy(true);
+        let index = tabs.indexOf(tab);
+        tab.$destroy();
 
-        if (tab.index === this.currentName) {
-          let nextChild = tabs[index];
-          let prevChild = tabs[index - 1];
-
-          while (prevChild && prevChild.disabled) {
-            prevChild = tabs[tabs.indexOf(prevChild) - 1];
-          }
-
-          this.currentName = nextChild
-            ? nextChild.index
-            : prevChild
-            ? prevChild.index
-            : '-1';
-        }
         this.$emit('tab-remove', tab);
         this.$forceUpdate();
-      },
-      handleTabClick(tab, event) {
-        if (tab.disabled) return;
-        this.currentName = tab.index;
-        this.$emit('tab-click', tab, event);
-      },
-      calcBarStyle() {
-        if (this.type || !this.$refs.tabs) return {};
-        var style = {};
-        var offset = 0;
-        var tabWidth = 0;
 
-        this.$children.every((panel, index) => {
+        this.$nextTick(_ => {
+          if (tab.active) {
+            let nextChild = tabs[index];
+            let prevChild = tabs[index - 1];
+            let nextActiveTab = nextChild || prevChild || null;
+
+            if (nextActiveTab) {
+              this.currentName = nextActiveTab.name || nextActiveTab.index;
+            }
+          }
+        });
+      },
+      handleTabClick(tab, tabName, event) {
+        if (tab.disabled) return;
+        this.currentName = tabName;
+        this.$emit('tab-click', tab, event);
+      }
+    },
+    mounted() {
+      this.$forceUpdate();
+    },
+    render(h) {
+      let {
+        type,
+        handleTabRemove,
+        handleTabClick,
+        currentName
+      } = this;
+
+      const getBarStyle = () => {
+        if (this.type || !this.$refs.tabs) return {};
+        let style = {};
+        let offset = 0;
+        let tabWidth = 0;
+
+        this.$children.every((tab, index) => {
           let $el = this.$refs.tabs[index];
           if (!$el) { return false; }
-          if (panel.index !== this.currentName) {
+
+          if (!tab.active) {
             offset += $el.clientWidth;
             return true;
           } else {
@@ -79,51 +94,45 @@
         style.transform = `translateX(${offset}px)`;
 
         return style;
-      }
-    },
-    mounted() {
-      this.currentName = this.activeName || this.$children[0] && this.$children[0].index || '1';
-      this.$nextTick(() => {
-        this.$forceUpdate();
-      });
-    },
-    render(h) {
-      let {
-        type,
-        panes, // eslint-disable-line
-        handleTabRemove,
-        handleTabClick,
-        currentName
-      } = this;
-
-      const barStyle = this.calcBarStyle();
-      const activeBar = !type
-        ? <div class="el-tabs__active-bar" style={barStyle}></div>
-        : null;
+      };
 
       const tabs = this.$children.map((tab, index) => {
-        let btnClose = h('span', {
-          class: {
-            'el-icon-close': true
-          },
-          on: { click: (ev) => { handleTabRemove(tab, ev); } }
-        });
-        const _tab = h('div', {
-          class: {
-            'el-tabs__item': true,
-            'is-active': currentName === tab.index,
-            'is-disabled': tab.disabled,
-            'is-closable': tab.isClosable
-          },
-          ref: 'tabs',
-          refInFor: true,
-          on: { click: (ev) => { handleTabClick(tab, ev); } }
-        }, [
-          tab.labelContent ? tab.labelContent.call(this._renderProxy, h, tab) : tab.label,
-          tab.isClosable ? btnClose : null,
-          index === 0 ? activeBar : null
-        ]);
-        return _tab;
+        let tabName = tab.name || tab.index || index;
+        if (currentName === undefined && index === 0) {
+          this.currentName = tabName;
+        }
+
+        tab.index = index;
+
+        const activeBar = !type && index === 0
+          ? <div class="el-tabs__active-bar" style={getBarStyle()}></div>
+          : null;
+
+        const btnClose = tab.isClosable
+          ? <span class="el-icon-close" on-click={(ev) => { handleTabRemove(tab, ev); }}></span>
+          : null;
+
+        const tabLabelContent = tab.labelContent
+          ? tab.labelContent.call(this._renderProxy, h, tab)
+          : tab.label;
+
+        return (
+          <div
+            class={{
+              'el-tabs__item': true,
+              'is-active': tab.active,
+              'is-disabled': tab.disabled,
+              'is-closable': tab.isClosable
+            }}
+            ref="tabs"
+            refInFor
+            on-click={(ev) => { handleTabClick(tab, tabName, ev); }}
+          >
+            {tabLabelContent}
+            {btnClose}
+            {activeBar}
+          </div>
+        );
       });
       return (
         <div class={{
