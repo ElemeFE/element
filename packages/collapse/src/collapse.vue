318c73efc1d007d4bce32e73708f8d494af3481e
baiyaaaaa <bigpet1991@gmail.com>
2016-12-25 18:25:18 +0800

    add collapse

--- packages/collapse/src/collapse.vue
+++ packages/collapse/src/collapse.vue
@@ -9,18 +9,52 @@
 
     componentName: 'ElCollapse',
 
+    props: {
+      accordion: Boolean,
+      value: {
+        type: [Array, String],
+        default() {
+          return [];
+        }
+      }
+    },
+
     data() {
       return {
+        activeNames: [].concat(this.value)
       };
     },
 
-    props: {
+    watch: {
+      value(value) {
+        this.activeNames = [].concat(value);
+      }
     },
 
     methods: {
+      setActiveItems(activeNames) {
+        this.activeNames = activeNames;
+        this.$emit('input', this.accordion ? activeNames[0] : activeNames);
+      },
+      handleItemClick(item) {
+        if (this.accordion) {
+          this.setActiveItems([].concat(this.activeNames[0] === item.name ? '' : item.name));
+        } else {
+          let activeNames = this.activeNames.slice(0);
+          let index = activeNames.indexOf(item.name);
+
+          if (index > -1) {
+            activeNames.splice(index, 1);
+          } else {
+            activeNames.push(item.name);
+          }
+          this.setActiveItems(activeNames);
+        }
+      }
     },
 
     created() {
+      this.$on('item-click', this.handleItemClick);
     }
   };
 </script>
