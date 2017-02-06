9dee7854655b573cf7debb83c8565474e0f74743
baiyaaaaa <bigpet1991@gmail.com>
2017-02-06 14:29:53 +0800

    autocomplete dropdown use el-scrollbar

--- packages/autocomplete/src/autocomplete-suggestions.vue
+++ packages/autocomplete/src/autocomplete-suggestions.vue
@@ -1,30 +1,36 @@
 <template>
   <transition name="el-zoom-in-top" @after-leave="doDestroy">
-    <ul
+    <div
       v-show="showPopper"
-      class="el-autocomplete__suggestions"
+      class="el-autocomplete-suggestion"
       :class="{ 'is-loading': parent.loading }"
       :style="{ width: dropdownWidth }"
     >
-      <li v-if="parent.loading"><i class="el-icon-loading"></i></li>
-      <template v-for="(item, index) in suggestions" v-else>
-        <li
-          v-if="!parent.customItem"
-          :class="{'highlighted': parent.highlightedIndex === index}"
-          @click="select(item)"
-        >
-          {{item.value}}
-        </li>
-        <component
-          v-else
-          :class="{'highlighted': parent.highlightedIndex === index}"
-          @click="select(item)"
-          :is="parent.customItem"
-          :item="item"
-          :index="index">
-        </component>
-      </template>
-    </ul>
+      <el-scrollbar
+        tag="ul"
+        wrap-class="el-autocomplete-suggestion__wrap"
+        view-class="el-autocomplete-suggestion__list"
+      >
+        <li v-if="parent.loading"><i class="el-icon-loading"></i></li>
+        <template v-for="(item, index) in suggestions" v-else>
+          <li
+            v-if="!parent.customItem"
+            :class="{'highlighted': parent.highlightedIndex === index}"
+            @click="select(item)"
+          >
+            {{item.value}}
+          </li>
+          <component
+            v-else
+            :class="{'highlighted': parent.highlightedIndex === index}"
+            @click="select(item)"
+            :is="parent.customItem"
+            :item="item"
+            :index="index">
+          </component>
+        </template>
+      </el-scrollbar>
+    </div>
   </transition>
 </template>
 <script>
