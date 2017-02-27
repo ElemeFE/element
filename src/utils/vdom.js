5545fea943057ad2e1d6071bbbc3f258e07b5977
qingwei.li <cinwell.li@gmail.com>
2017-02-27 11:07:15 +0800

    support vue 2.2

--- src/utils/vdom.js
+++ src/utils/vdom.js
@@ -1,7 +1,7 @@
-import Vue from 'vue';
+import { hasOwn } from 'element-ui/src/utils/util';
 
 export function isVNode(node) {
-  return typeof node === 'object' && Vue.util.hasOwn(node, 'componentOptions');
+  return typeof node === 'object' && hasOwn(node, 'componentOptions');
 };
 
 export function getFirstComponentChild(children) {
