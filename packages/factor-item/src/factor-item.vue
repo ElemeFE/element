4bde639658a675ad1c4353327a95debcb296a859
liangyj021 <617062781@qq.com>
2017-03-10 17:44:49 +0800

    add new components: factor-item/ suspect-item

---
+++ packages/factor-item/src/factor-item.vue
@@ -0,0 +1,10 @@
+<template>
+</template>
+<script>
+  export default {
+    name: 'ElFactorItem',
+    props: {},
+    data() {},
+    methods: {}
+  };
+</script>
---
+++ packages/suspect-item/src/suspect-item.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+<script>
+  export default {
+    name: 'ElSuspectItem',
+
+    props: {},
+
+    data() {},
+
+    methods: {}
+
+  };
+</script>
