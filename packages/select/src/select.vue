62332f07ce074cc7d6174e1bf5bb655756fcf22d
Leopoldthecoder <Leopoldthecuber@gmail.com>
2017-03-27 16:17:19 +0800

    Select: exec removeResizeListener in beforeDestroy hook, fixed #3797

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -665,8 +665,8 @@
       });
     },
 
-    destroyed() {
-      if (this.handleResize) removeResizeListener(this.$el, this.handleResize);
+    beforeDestroy() {
+      if (this.$el && this.handleResize) removeResizeListener(this.$el, this.handleResize);
     }
   };
 </script>
