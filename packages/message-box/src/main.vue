09345133b98a199baeba45f27faab21a6960e488
qingwei.li <cinwell.li@gmail.com>
2017-03-29 20:05:39 +0800

    MessageBox: add enter event for prompt

--- packages/message-box/src/main.vue
+++ packages/message-box/src/main.vue
@@ -10,7 +10,7 @@
           <div class="el-message-box__status" :class="[ typeClass ]"></div>
           <div class="el-message-box__message" :style="{ 'margin-left': typeClass ? '50px' : '0' }"><p>{{ message }}</p></div>
           <div class="el-message-box__input" v-show="showInput">
-            <el-input v-model="inputValue" :placeholder="inputPlaceholder" ref="input"></el-input>
+            <el-input v-model="inputValue" @keyup.enter.native="handleAction('confirm')" :placeholder="inputPlaceholder" ref="input"></el-input>
             <div class="el-message-box__errormsg" :style="{ visibility: !!editorErrorMessage ? 'visible' : 'hidden' }">{{ editorErrorMessage }}</div>
           </div>
         </div>
