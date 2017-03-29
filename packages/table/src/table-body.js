a3273188e7ffaeac2ae508603231c8f3e153d31f
cinwell.li <cinwell.li@gmail.com>
2017-03-29 11:59:12 +0800

    Table: improve performance for rendering tooltip, fixed #3478 (#3836)
    * Table: improve performance for rendering tooltip, fixed #3478
    
    * Table: add tooltip-effect property

--- packages/table/src/table-body.js
+++ packages/table/src/table-body.js
@@ -1,5 +1,6 @@
 import { getCell, getColumnByCell, getRowIdentity } from './util';
 import ElCheckbox from 'element-ui/packages/checkbox';
+import debounce from 'throttle-debounce/debounce';
 
 export default {
   components: {
@@ -73,6 +74,8 @@ export default {
                   </tr>)
                 : ''
               ]
+            ).concat(
+              <el-tooltip effect={ this.table.tooltipEffect } placement="top" ref="tooltip" content={ this.tooltipContent }></el-tooltip>
             )
           }
         </tbody>
@@ -142,8 +145,12 @@ export default {
 
   data() {
     return {
-      tooltipDisabled: true
+      tooltipContent: ''
     };
+  },
+
+  created() {
+    this.activateTooltip = debounce(50, tooltip => tooltip.handleShowPopper());
   },
 
   methods: {
@@ -199,10 +206,18 @@ export default {
       // 判断是否text-overflow, 如果是就显示tooltip
       const cellChild = event.target.querySelector('.cell');
 
-      this.tooltipDisabled = cellChild.scrollWidth <= cellChild.offsetWidth;
+      if (cellChild.scrollWidth > cellChild.offsetWidth) {
+        const tooltip = this.$refs.tooltip;
+
+        this.tooltipContent = cell.innerText;
+        tooltip.referenceElm = cell;
+        tooltip.doDestroy();
+        this.activateTooltip(tooltip);
+      }
     },
 
     handleCellMouseLeave(event) {
+      this.$refs.tooltip.handleClosePopper();
       const cell = getCell(event);
       if (!cell) return;
 
