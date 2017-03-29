bab0581f7ca71d3697d95b4020751569a4f0d9ce
qingwei.li <cinwell.li@gmail.com>
2017-03-29 10:55:42 +0800

    Scrollbar: compatible with vue 2.2.6, fixed #3797

--- packages/scrollbar/src/main.js
+++ packages/scrollbar/src/main.js
@@ -123,7 +123,7 @@ export default {
     !this.noresize && addResizeListener(this.$refs.resize, this.update);
   },
 
-  destroyed() {
+  beforeDestroy() {
     if (this.native) return;
     !this.noresize && removeResizeListener(this.$refs.resize, this.update);
   }
