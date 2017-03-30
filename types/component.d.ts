c0dd945edf7cd19b06a599090501fc15626a8daa
hjkcai <huajingkun1214@gmail.com>
2017-03-30 15:07:29 +0800

    add common component definition

---
+++ types/component.d.ts
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+/** ElementUI component common definition */
+export class ElementUIComponent extends Vue {
+  /** Install component into Vue */
+  static install (vue: typeof Vue)
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import { ElementUIComponent } from './component'
 
 declare namespace ElementUI {
   export interface InstallationOptions {
@@ -6,7 +7,15 @@ declare namespace ElementUI {
     i18n: any
   }
 
+  /**
+   * Install all element-ui components into Vue.
+   * Please do not invoke this method directly.
+   * Call `Vue.use(ElementUI)` to install.
+   **/
   export function install (vue: typeof Vue, options: ElementUI.InstallationOptions)
+
+  /** ElementUI component common definition */
+  export type Component = ElementUIComponent
 }
 
 export = ElementUI
