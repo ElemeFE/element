e064b78e9bafb13b29571446dd7520607d3d091e
Leopoldthecoder <Leopoldthecuber@gmail.com>
2017-05-05 08:37:30 +0800

    [build] 1.3.0

--- src/index.js
+++ src/index.js
@@ -158,7 +158,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 };
 
 module.exports = {
-  version: '1.3.0-beta.3',
+  version: '1.3.0',
   locale: locale.use,
   i18n: locale.i18n,
   install,
