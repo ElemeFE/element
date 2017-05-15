6c5775b4159b3c639d6be5f5c18f14ba05fb4cf6
jindiying <jindiying@jindiyingdeiMac.local>
2017-05-15 19:18:53 +0800

    Change padding.

--- packages/menu/src/menu-mixin.js
+++ packages/menu/src/menu-mixin.js
@@ -34,11 +34,11 @@ export default {
     paddingStyle() {
       if (this.rootMenu.mode !== 'vertical') return {};
 
-      let padding = 20;
+      let padding = 10;
       let parent = this.$parent;
       while (parent && parent.$options.componentName !== 'ElMenu') {
         if (parent.$options.componentName === 'ElSubmenu') {
-          padding += 20;
+          padding += 10;
         }
         parent = parent.$parent;
       }
