af17bd6ad3d74adcb4e58abdd80fffcfffd39061
Kermo <kermo@vedru.eu>
2017-06-08 11:20:35 +0300

    Add Estonian language

---
+++ src/locale/lang/ee.js
@@ -0,0 +1,104 @@
+export default {
+  el: {
+    colorpicker: {
+      confirm: 'OK',
+      clear: 'Tühjenda'
+    },
+    datepicker: {
+      now: 'Praegu',
+      today: 'Täna',
+      cancel: 'Tühista',
+      clear: 'Tühjenda',
+      confirm: 'OK',
+      selectDate: 'Vali kuupäev',
+      selectTime: 'Vali kellaaeg',
+      startDate: 'Alguskuupäev',
+      startTime: 'Algusaeg',
+      endDate: 'Lõpukuupäev',
+      endTime: 'Lõpuaeg',
+      year: '',
+      month1: 'Jaanuar',
+      month2: 'Veebruar',
+      month3: 'Märts',
+      month4: 'Aprill',
+      month5: 'Mai',
+      month6: 'Juuni',
+      month7: 'Juuli',
+      month8: 'August',
+      month9: 'September',
+      month10: 'Oktoober',
+      month11: 'November',
+      month12: 'Detsember',
+      // week: 'nädal',
+      weeks: {
+        sun: 'P',
+        mon: 'E',
+        tue: 'T',
+        wed: 'K',
+        thu: 'N',
+        fri: 'R',
+        sat: 'L'
+      },
+      months: {
+        jan: 'Jaan',
+        feb: 'Veeb',
+        mar: 'Mär',
+        apr: 'Apr',
+        may: 'Mai',
+        jun: 'Juun',
+        jul: 'Juul',
+        aug: 'Aug',
+        sep: 'Sept',
+        oct: 'Okt',
+        nov: 'Nov',
+        dec: 'Dets'
+      }
+    },
+    select: {
+      loading: 'Laadimine',
+      noMatch: 'Sobivad andmed puuduvad',
+      noData: 'Andmed puuduvad',
+      placeholder: 'Vali'
+    },
+    cascader: {
+      noMatch: 'Sobivad andmed puuduvad',
+      loading: 'Laadimine',
+      placeholder: 'Vali'
+    },
+    pagination: {
+      goto: 'Mine lehele',
+      pagesize: '/page',
+      total: 'Kokku {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'Teade',
+      confirm: 'OK',
+      cancel: 'Tühista',
+      error: 'Vigane sisend'
+    },
+    upload: {
+      delete: 'Kustuta',
+      preview: 'Eelvaate',
+      continue: 'Jätka'
+    },
+    table: {
+      emptyText: 'Andmed puuduvad',
+      confirmFilter: 'Kinnita',
+      resetFilter: 'Taasta',
+      clearFilter: 'Kõik',
+      sumText: 'Summa'
+    },
+    tree: {
+      emptyText: 'Andmed puuduvad'
+    },
+    transfer: {
+      noMatch: 'Sobivad andmed puuduvad',
+      noData: 'Andmed puuduvad',
+      titles: ['List 1', 'List 2'], // to be translated
+      filterPlaceholder: 'Enter keyword', // to be translated
+      noCheckedFormat: '{total} items', // to be translated
+      hasCheckedFormat: '{checked}/{total} checked' // to be translated
+    }
+  }
+};
