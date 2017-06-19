933b2e83af3e26afe957d6abeea5ab7d94865498
Jerre Baumeister <jerre.bau@gmail.com>
2017-06-19 22:06:07 +0200

    Message: add 'message' support for VNode

--- packages/message/src/main.js
+++ packages/message/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import { PopupManager } from 'element-ui/src/utils/popup';
+import { isVNode } from 'element-ui/src/utils/vdom';
 let MessageConstructor = Vue.extend(require('./main.vue'));
 
 let instance;
@@ -20,11 +21,14 @@ var Message = function(options) {
   options.onClose = function() {
     Message.close(id, userOnClose);
   };
-
   instance = new MessageConstructor({
     data: options
   });
   instance.id = id;
+  if (isVNode(instance.message)) {
+    instance.$slots.default = [instance.message];
+    instance.message = null;
+  }
   instance.vm = instance.$mount();
   document.body.appendChild(instance.vm.$el);
   instance.vm.visible = true;
