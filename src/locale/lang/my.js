56821ec808b9e2afe9356000e4d1074eb554e4af
Kaung Myat Lwin <kaungmyatlwin@nexlabs.co>
2017-07-17 17:07:59 +0630

    [Language Support] Add Myanmar Language

---
+++ src/locale/lang/my.js
@@ -0,0 +1,104 @@
+export default {
+  el: {
+    colorpicker: {
+      confirm: 'အတည်ပြု',
+      clear: 'ရှင်းလင်း'
+    },
+    datepicker: {
+      now: 'ယခု',
+      today: 'ယနေ့',
+      cancel: 'ဖျက်သိမ်း',
+      clear: 'ရှင်းလင်း',
+      confirm: 'အတည်ပြု',
+      selectDate: 'နေ့စွဲရွေးရန်',
+      selectTime: 'အချိန်ရွေးရန်',
+      startDate: 'အစပြုရက်',
+      startTime: 'အစပြုချိန်',
+      endDate: 'ပြီးဆုံးရက်',
+      endTime: 'ပြီးဆုံးချိန်',
+      year: 'နှစ်',
+      month1: 'ဇန်နဝါရီလ',
+      month2: 'ဖေဖေါ်ဝါရီလ',
+      month3: 'မတ်လ',
+      month4: 'ဧပြီလ',
+      month5: 'မေလ',
+      month6: 'ဇွန်လ',
+      month7: 'ဇူလိုင်လ',
+      month8: 'သြဂုတ်လ',
+      month9: 'စက်တင်ဘာလ',
+      month10: 'အောက်တိုဘာလ',
+      month11: 'နို၀င်ဘာလ',
+      month12: 'ဒီဇင်ဘာလ',
+      // week: 'week',
+      weeks: {
+        sun: 'တနင်္ဂနွေ',
+        mon: 'တနင်္လာ',
+        tue: 'အင်္ဂါ',
+        wed: 'ဗုဒ္ဓဟူး',
+        thu: 'ကြာသပတေး',
+        fri: 'သောကြာ',
+        sat: 'စနေ'
+      },
+      months: {
+        jan: 'ဇန်နဝါရီ',
+        feb: 'ဖေဖေါ်ဝါရီ',
+        mar: 'မတ်',
+        apr: 'ဧပြီ',
+        may: 'မေ',
+        jun: 'ဇွန်',
+        jul: 'ဇူလိုင်',
+        aug: 'သြဂုတ်',
+        sep: 'စက်တင်ဘာ',
+        oct: 'အောက်တိုဘာ',
+        nov: 'နို၀င်ဘာ',
+        dec: 'ဒီဇင်ဘာ'
+      }
+    },
+    select: {
+      loading: 'ခဏစောင့်ပါ',
+      noMatch: 'အချက်အလက် မတွေ့ပါ',
+      noData: 'အချက်အလက် မရှိပါ',
+      placeholder: 'ရွေးချယ်ရန်'
+    },
+    cascader: {
+      noMatch: 'အချက်အလက် မတွေ့ပါ',
+      loading: 'ခဏစောင့်ပါ',
+      placeholder: 'ရွေးချယ်ရန်'
+    },
+    pagination: {
+      goto: 'သွားရန်',
+      pagesize: '/page',
+      total: 'စုစုပေါင်း {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'စာတို',
+      confirm: 'အတည်ပြု',
+      cancel: 'ဖျက်ရန်',
+      error: 'အကျုံးမ၀င်ပါ'
+    },
+    upload: {
+      delete: 'ဖျက်ရန်',
+      preview: 'ကြည့်ရန်',
+      continue: 'ဆက်လုပ်ရန်'
+    },
+    table: {
+      emptyText: '',
+      confirmFilter: 'အတည်ပြု',
+      resetFilter: 'Filter များ ပြင်ဆင်ရန်',
+      clearFilter: 'Filter များ ဖယ်ရှားရန်',
+      sumText: 'စုစုပေါင်း'
+    },
+    tree: {
+      emptyText: 'မရှိပါ'
+    },
+    transfer: {
+      noMatch: 'အချက်အလက် မတွေ့ပါ',
+      noData: 'အချက်အလက် မရှိပါ',
+      titles: ['List 1', 'List 2'], // to be translated
+      filterPlaceholder: 'Enter keyword', // to be translated
+      noCheckedFormat: '{total} items', // to be translated
+      hasCheckedFormat: '{checked}/{total} checked' // to be translated
+    }
+  }
+};
