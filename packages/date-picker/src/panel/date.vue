be6cc7800477f0b34132b3e575c8b1b153f7d054
ChuckFields <theclowncharliebrown@gmail.com>
2017-07-20 02:15:03 -0400

    DateTimePicker: Fixes DateTinePicker format is not taken into the picker itself #4931 (#5293)
    * DatePicker: Fix Typing a date and tabbing does not close the calendar #4948
    
    * DatePicker: Fix Typing a date and tabbing does not close the calendar #4948 -Updated test case
    
    * Fixes #4931

--- packages/date-picker/src/panel/date.vue
+++ packages/date-picker/src/panel/date.vue
@@ -406,11 +406,11 @@
 
       visibleDate: {
         get() {
-          return formatDate(this.date);
+          return formatDate(this.date, this.dateFormat);
         },
 
         set(val) {
-          const date = parseDate(val, 'yyyy-MM-dd');
+          const date = parseDate(val, this.dateFormat);
           if (!date) {
             return;
           }
@@ -444,6 +444,14 @@
           return 'HH:mm';
         } else {
           return 'HH:mm:ss';
+        }
+      },
+
+      dateFormat() {
+        if (this.format) {
+          return this.format.replace('HH:mm', '').replace(':ss', '').trim();
+        } else {
+          return 'yyyy-MM-dd';
         }
       }
     }
