f183376e1294a7eb3376507bd0b3767930994814
wuli <wuli@howzhi.com>
2017-08-01 07:54:55 +0800

    cascader 当children长度为0时,不显示空白的下级菜单

--- packages/cascader/src/menu.vue
+++ packages/cascader/src/menu.vue
@@ -123,7 +123,7 @@
           if (item.__IS__FLAT__OPTIONS) isFlat = true;
 
           if (!item.disabled) {
-            if (item.children.length) {
+            if (item.children && item.children.length) {
               let triggerEvent = {
                 click: 'click',
                 hover: 'mouseenter'
@@ -148,7 +148,7 @@
             <li
               class={{
                 'el-cascader-menu__item': true,
-                'el-cascader-menu__item--extensible': item.children.length,
+                'el-cascader-menu__item--extensible': item.children && item.children.length,
                 'is-active': item.value === activeValue[menuIndex],
                 'is-disabled': item.disabled
               }}
