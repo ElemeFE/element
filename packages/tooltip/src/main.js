8c0191d9f86432e81bf4f35aa51c7f820c758797
ryatziv <rom.yatziv@graymeta.com>
2017-08-10 10:01:18 -0700

    added hide-after timeout for hiding tooltip

--- packages/tooltip/src/main.js
+++ packages/tooltip/src/main.js
@@ -39,7 +39,17 @@ export default {
     enterable: {
       type: Boolean,
       default: true
+    },
+    hideAfter: {
+      type: Number,
+      default: 0
     }
+  },
+
+  data() {
+    return {
+      timeoutPending: null
+    };
   },
 
   beforeCreate() {
@@ -111,15 +121,26 @@ export default {
       this.timeout = setTimeout(() => {
         this.showPopper = true;
       }, this.openDelay);
+
+      if (this.hideAfter > 0) {
+        this.timeoutPending = setTimeout(() => {
+          this.showPopper = false;
+        }, this.hideAfter);
+      }
     },
 
     handleClosePopper() {
       if (this.enterable && this.expectedState || this.manual) return;
-      clearTimeout(this.timeout);
+      if (this.timeoutPending) {
+        clearTimeout(this.timeoutPending);
+      }
       this.showPopper = false;
     },
 
     setExpectedState(expectedState) {
+      if (expectedState === false) {
+        clearTimeout(this.timeoutPending);
+      }
       this.expectedState = expectedState;
     }
   }
