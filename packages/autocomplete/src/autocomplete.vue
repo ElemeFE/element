5268a8f8c16a0c584bd32f8f7ba0783b0f880653
leezng <294795201@qq.com>
2017-08-15 13:30:12 +0800

    Autocomplete: Don't prevent form submit

--- packages/autocomplete/src/autocomplete.vue
+++ packages/autocomplete/src/autocomplete.vue
@@ -10,7 +10,7 @@
       @focus="handleFocus"
       @keydown.up.native.prevent="highlight(highlightedIndex - 1)"
       @keydown.down.native.prevent="highlight(highlightedIndex + 1)"
-      @keydown.enter.native.prevent="handleKeyEnter"
+      @keydown.enter.native="handleKeyEnter"
       @keydown.native.tab="close"
     >
       <template slot="prepend" v-if="$slots.prepend">
@@ -133,8 +133,9 @@
       close(e) {
         this.activated = false;
       },
-      handleKeyEnter() {
+      handleKeyEnter(e) {
         if (this.suggestionVisible && this.highlightedIndex >= 0 && this.highlightedIndex < this.suggestions.length) {
+          e.preventDefault()
           this.select(this.suggestions[this.highlightedIndex]);
         }
       },
