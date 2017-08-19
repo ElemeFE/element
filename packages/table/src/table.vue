174761117de102f91ab587392e58ff5ebd76e9fe
杨奕 <Leopoldthecoder@users.noreply.github.com>
2017-08-19 22:22:19 +0800

    Update table.vue

--- packages/table/src/table.vue
+++ packages/table/src/table.vue
@@ -257,7 +257,9 @@
         });
 
         const scrollBodyWrapper = event => {
-          const deltaX = event.deltaX;
+          const { deltaX, deltaY } = event;
+
+          if (Math.abs(deltaX) < Math.abs(deltaY)) return;
 
           if (deltaX > 0) {
             this.bodyWrapper.scrollLeft += 10;
