58e34d1bae1d28165421f1d08d543e1d6109e33f
Leopoldthecoder <Leopoldthecuber@gmail.com>
2017-08-21 15:54:41 +0800

    fix test

--- packages/container/src/main.vue
+++ packages/container/src/main.vue
@@ -21,10 +21,12 @@
         } else if (this.direction === 'horizontal') {
           return false;
         }
-        return this.$slots.default.some(vnode => {
-          const tag = vnode.componentOptions && vnode.componentOptions.tag;
-          return tag === 'el-header' || tag === 'el-footer';
-        });
+        return this.$slots && this.$slots.default
+          ? this.$slots.default.some(vnode => {
+            const tag = vnode.componentOptions && vnode.componentOptions.tag;
+            return tag === 'el-header' || tag === 'el-footer';
+          })
+          : false;
       }
     }
   };
