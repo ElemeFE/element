0de6b447e5ee065e6bd768efd78bc292d99d01a3
Brad Adams <bread.adams@gmail.com>
2017-10-09 14:07:10 +0200

    Table: Add `important` rule to `col-resize` cursor (#7381)
    * Table: Add `important` rule to `col-resize` cursor
    
    * Table: Add `important` rule to `col-resize` cursor
    
    * Update table-header.js

--- packages/table/src/table-header.js
+++ packages/table/src/table-header.js
@@ -369,9 +369,15 @@ export default {
         const bodyStyle = document.body.style;
         if (rect.width > 12 && rect.right - event.pageX < 8) {
           bodyStyle.cursor = 'col-resize';
+          if (hasClass(target, 'is-sortable')) {
+            target.style.cursor = 'col-resize';
+          }
           this.draggingColumn = column;
         } else if (!this.dragging) {
           bodyStyle.cursor = '';
+          if (hasClass(target, 'is-sortable')) {
+            target.style.cursor = 'pointer';
+          }
           this.draggingColumn = null;
         }
       }
