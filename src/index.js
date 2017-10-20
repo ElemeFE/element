44c75ff1af0ba27cdadc1aea330393039a5fa174
Leopoldthecoder <Leopoldthecuber@gmail.com>
2017-10-20 16:09:07 +0800

    [build] 2.0.0-beta.1

--- src/index.js
+++ src/index.js
@@ -173,7 +173,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 };
 
 module.exports = {
-  version: '2.0.0-alpha.3',
+  version: '2.0.0-beta.1',
   locale: locale.use,
   i18n: locale.i18n,
   install,
