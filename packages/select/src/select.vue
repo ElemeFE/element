5d172ad5cffae52ea028855641ce8f110baddcac
Leopoldthecoder <Leopoldthecuber@gmail.com>
2017-10-31 10:37:32 +0800

    Select: fix input height of mini size

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -540,7 +540,7 @@
           let input = [].filter.call(inputChildNodes, item => item.tagName === 'INPUT')[0];
           const tags = this.$refs.tags;
           input.style.height = this.selected.length === 0 && this.selectSize === 'mini'
-            ? sizeMap[this.selectSize]
+            ? sizeMap[this.selectSize] + 'px'
             : Math.max(tags ? (tags.clientHeight + 10) : 0, sizeMap[this.selectSize] || 40) + 'px';
           if (this.visible && this.emptyText !== false) {
             this.broadcast('ElSelectDropdown', 'updatePopper');
