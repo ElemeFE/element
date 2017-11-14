4f00d7dea84c5b38c686c2f700520fc6ec59dca0
lyh2668 <lyh2668@163.com>
2017-11-14 22:26:39 +0800

    Update table-body.js
    添加一个this.$refs.tooltip是否存在的判断

--- packages/table/src/table-body.js
+++ packages/table/src/table-body.js
@@ -340,7 +340,7 @@ export default {
       // 判断是否text-overflow, 如果是就显示tooltip
       const cellChild = event.target.querySelector('.cell');
 
-      if (hasClass(cellChild, 'el-tooltip') && cellChild.scrollWidth > cellChild.offsetWidth) {
+      if (hasClass(cellChild, 'el-tooltip') && cellChild.scrollWidth > cellChild.offsetWidth && this.$refs.tooltip) {
         const tooltip = this.$refs.tooltip;
 
         this.tooltipContent = cell.innerText;
