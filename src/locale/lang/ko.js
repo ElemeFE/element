1af6989544c9b7e1c8ab2a04159423eb29066f00
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-11-17 13:14:06 +0900

    Update ko.js (#8348)

--- src/locale/lang/ko.js
+++ src/locale/lang/ko.js
@@ -82,7 +82,7 @@ export default {
       error: '올바르지 않은 입력'
     },
     upload: {
-      deleteTip: 'press delete to remove',
+      deleteTip: '클릭시 삭제됩니다'
       delete: '삭제',
       preview: '미리보기',
       continue: '계속하기'
