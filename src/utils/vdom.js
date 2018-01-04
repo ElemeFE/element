e466b32214ab93b4efd7db4dec9e9387c728b774
xifeiwu <sduwxf@qq.com>
2018-01-04 15:46:55 +0800

    fix the case parameter node is null (#9125)

--- src/utils/vdom.js
+++ src/utils/vdom.js
@@ -1,7 +1,7 @@
 import { hasOwn } from 'element-ui/src/utils/util';
 
 export function isVNode(node) {
-  return typeof node === 'object' && hasOwn(node, 'componentOptions');
+  return node !== null && typeof node === 'object' && hasOwn(node, 'componentOptions');
 };
 
 export function getFirstComponentChild(children) {
