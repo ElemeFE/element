56f70c8646ae43a352759e1502d1143137185999
Anatoly Kolesov <kolesoffac@gmail.com>
2018-01-09 11:55:47 +0300

    remove space

--- packages/loading/src/directive.js
+++ packages/loading/src/directive.js
@@ -44,10 +44,8 @@ exports.install = Vue => {
             : el;
           removeClass(target, 'el-loading-parent--relative');
           removeClass(target, 'el-loading-parent--hidden');
-          
           el.instance.hiding = false;
         });
-
         el.instance.visible = false;
         el.instance.hiding = true;
       }
