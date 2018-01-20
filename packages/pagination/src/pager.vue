c7cee97bba1a588d0261bcd563251d645bf8f138
chongjohn716 <chong.john716@gmail.com>
2018-01-21 05:25:03 +0800

    Pagination: add pager-count property

--- packages/pagination/src/pager.vue
+++ packages/pagination/src/pager.vue
@@ -26,123 +26,123 @@
 </template>
 
 <script type="text/babel">
-export default {
-  name: 'ElPager',
+  export default {
+    name: 'ElPager',
 
-  props: {
-    currentPage: Number,
+    props: {
+      currentPage: Number,
 
-    pageCount: Number,
+      pageCount: Number,
 
-    pagerCount: Number
-  },
-
-  watch: {
-    showPrevMore(val) {
-      if (!val) this.quickprevIconClass = 'el-icon-more';
+      pagerCount: Number
     },
 
-    showNextMore(val) {
-      if (!val) this.quicknextIconClass = 'el-icon-more';
+    watch: {
+      showPrevMore(val) {
+        if (!val) this.quickprevIconClass = 'el-icon-more';
+      },
+
+      showNextMore(val) {
+        if (!val) this.quicknextIconClass = 'el-icon-more';
+      }
+    },
+
+    methods: {
+      onPagerClick(event) {
+        const target = event.target;
+        const pagerOffset = this.pagerCount - 2;
+
+        if (target.tagName === 'UL') {
+          return;
+        }
+
+        let newPage = Number(event.target.textContent);
+        const pageCount = this.pageCount;
+        const currentPage = this.currentPage;
+
+        if (target.className.indexOf('more') !== -1) {
+          if (target.className.indexOf('quickprev') !== -1) {
+            newPage = currentPage - pagerOffset;
+          } else if (target.className.indexOf('quicknext') !== -1) {
+            newPage = currentPage + pagerOffset;
+          }
+        }
+
+        /* istanbul ignore if */
+        if (!isNaN(newPage)) {
+          if (newPage < 1) {
+            newPage = 1;
+          }
+
+          if (newPage > pageCount) {
+            newPage = pageCount;
+          }
+        }
+
+        if (newPage !== currentPage) {
+          this.$emit('change', newPage);
+        }
+      }
+    },
+
+    computed: {
+      pagers() {
+        const pagerCount = this.pagerCount;
+        const halfPagerNumber = (pagerCount - 1) / 2;
+
+        const currentPage = Number(this.currentPage);
+        const pageCount = Number(this.pageCount);
+
+        let showPrevMore = false;
+        let showNextMore = false;
+
+        if (pageCount > pagerCount) {
+          if (currentPage > pagerCount - halfPagerNumber) {
+            showPrevMore = true;
+          }
+
+          if (currentPage < pageCount - halfPagerNumber) {
+            showNextMore = true;
+          }
+        }
+
+        const array = [];
+
+        if (showPrevMore && !showNextMore) {
+          const startPage = pageCount - (pagerCount - 2);
+          for (let i = startPage; i < pageCount; i++) {
+            array.push(i);
+          }
+        } else if (!showPrevMore && showNextMore) {
+          for (let i = 2; i < pagerCount; i++) {
+            array.push(i);
+          }
+        } else if (showPrevMore && showNextMore) {
+          const offset = Math.floor(pagerCount / 2) - 1;
+          for (let i = currentPage - offset; i <= currentPage + offset; i++) {
+            array.push(i);
+          }
+        } else {
+          for (let i = 2; i < pageCount; i++) {
+            array.push(i);
+          }
+        }
+
+        this.showPrevMore = showPrevMore;
+        this.showNextMore = showNextMore;
+
+        return array;
+      }
+    },
+
+    data() {
+      return {
+        current: null,
+        showPrevMore: false,
+        showNextMore: false,
+        quicknextIconClass: 'el-icon-more',
+        quickprevIconClass: 'el-icon-more'
+      };
     }
-  },
-
-  methods: {
-    onPagerClick(event) {
-      const target = event.target;
-      const pagerOffset = this.pagerCount - 2;
-
-      if (target.tagName === 'UL') {
-        return;
-      }
-
-      let newPage = Number(event.target.textContent);
-      const pageCount = this.pageCount;
-      const currentPage = this.currentPage;
-
-      if (target.className.indexOf('more') !== -1) {
-        if (target.className.indexOf('quickprev') !== -1) {
-          newPage = currentPage - pagerOffset;
-        } else if (target.className.indexOf('quicknext') !== -1) {
-          newPage = currentPage + pagerOffset;
-        }
-      }
-
-      /* istanbul ignore if */
-      if (!isNaN(newPage)) {
-        if (newPage < 1) {
-          newPage = 1;
-        }
-
-        if (newPage > pageCount) {
-          newPage = pageCount;
-        }
-      }
-
-      if (newPage !== currentPage) {
-        this.$emit('change', newPage);
-      }
-    }
-  },
-
-  computed: {
-    pagers() {
-      const pagerCount = this.pagerCount;
-      const halfPagerNumber = (pagerCount - 1) / 2;
-
-      const currentPage = Number(this.currentPage);
-      const pageCount = Number(this.pageCount);
-
-      let showPrevMore = false;
-      let showNextMore = false;
-
-      if (pageCount > pagerCount) {
-        if (currentPage > pagerCount - halfPagerNumber) {
-          showPrevMore = true;
-        }
-
-        if (currentPage < pageCount - halfPagerNumber) {
-          showNextMore = true;
-        }
-      }
-
-      const array = [];
-
-      if (showPrevMore && !showNextMore) {
-        const startPage = pageCount - (pagerCount - 2);
-        for (let i = startPage; i < pageCount; i++) {
-          array.push(i);
-        }
-      } else if (!showPrevMore && showNextMore) {
-        for (let i = 2; i < pagerCount; i++) {
-          array.push(i);
-        }
-      } else if (showPrevMore && showNextMore) {
-        const offset = Math.floor(pagerCount / 2) - 1;
-        for (let i = currentPage - offset; i <= currentPage + offset; i++) {
-          array.push(i);
-        }
-      } else {
-        for (let i = 2; i < pageCount; i++) {
-          array.push(i);
-        }
-      }
-
-      this.showPrevMore = showPrevMore;
-      this.showNextMore = showNextMore;
-
-      return array;
-    }
-  },
-
-  data() {
-    return {
-      current: null,
-      showPrevMore: false,
-      showNextMore: false,
-      quicknextIconClass: 'el-icon-more',
-      quickprevIconClass: 'el-icon-more'
-    };
-  }
-};
+  };
 </script>
