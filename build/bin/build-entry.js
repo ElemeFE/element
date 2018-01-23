81a7ef76341fe73f0f4b8abaa3016da497eaabbb
Leopoldthecoder <Leopoldthecuber@gmail.com>
2018-01-23 18:25:49 +0800

    Dialog: update paddings

--- build/bin/build-entry.js
+++ build/bin/build-entry.js
@@ -46,7 +46,7 @@ const install = function(Vue, opts = {}) {
 /* istanbul ignore if */
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue);
-};
+}
 
 module.exports = {
   version: '{{version}}',
