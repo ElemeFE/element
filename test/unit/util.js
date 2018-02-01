3c5961cdea20baa4ef27ec07ffad7252d8e84121
wangyi7099 <wy564654798@126.com>
2018-02-01 15:04:42 +0800

    replace initEvent with new Event

--- test/unit/util.js
+++ test/unit/util.js
@@ -64,18 +64,9 @@ exports.createTest = function(Compo, propsData = {}, mounted = false) {
  * @param  {*} opts
  */
 exports.triggerEvent = function(elm, name, ...opts) {
-  let eventName;
-
-  if (/^mouse|click/.test(name)) {
-    eventName = 'MouseEvents';
-  } else if (/^key/.test(name)) {
-    eventName = 'KeyboardEvent';
-  } else {
-    eventName = 'HTMLEvents';
-  }
-  const evt = document.createEvent(eventName);
-
-  evt.initEvent(name, ...opts);
+  
+  const evt = new Event(name, ...opts);
+  
   elm.dispatchEvent
     ? elm.dispatchEvent(evt)
     : elm.fireEvent('on' + name, evt);
