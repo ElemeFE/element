d10c288abab28df9cd2060320990e9f10acf36da
qingwei.li <cinwell.li@gmail.com>
2018-02-27 11:58:14 +0800

    feat(tansfer): target value added to top, fixed #9882

--- packages/transfer/src/main.vue
+++ packages/transfer/src/main.vue
@@ -42,158 +42,165 @@
 </template>
 
 <script>
-  import ElButton from 'element-ui/packages/button';
-  import Emitter from 'element-ui/src/mixins/emitter';
-  import Locale from 'element-ui/src/mixins/locale';
-  import TransferPanel from './transfer-panel.vue';
-  import Migrating from 'element-ui/src/mixins/migrating';
+import ElButton from 'element-ui/packages/button';
+import Emitter from 'element-ui/src/mixins/emitter';
+import Locale from 'element-ui/src/mixins/locale';
+import TransferPanel from './transfer-panel.vue';
+import Migrating from 'element-ui/src/mixins/migrating';
 
-  export default {
-    name: 'ElTransfer',
+export default {
+  name: 'ElTransfer',
 
-    mixins: [Emitter, Locale, Migrating],
+  mixins: [Emitter, Locale, Migrating],
 
-    components: {
-      TransferPanel,
-      ElButton
-    },
+  components: {
+    TransferPanel,
+    ElButton
+  },
 
-    props: {
-      data: {
-        type: Array,
-        default() {
-          return [];
-        }
-      },
-      titles: {
-        type: Array,
-        default() {
-          return [];
-        }
-      },
-      buttonTexts: {
-        type: Array,
-        default() {
-          return [];
-        }
-      },
-      filterPlaceholder: {
-        type: String,
-        default: ''
-      },
-      filterMethod: Function,
-      leftDefaultChecked: {
-        type: Array,
-        default() {
-          return [];
-        }
-      },
-      rightDefaultChecked: {
-        type: Array,
-        default() {
-          return [];
-        }
-      },
-      renderContent: Function,
-      value: {
-        type: Array,
-        default() {
-          return [];
-        }
-      },
-      format: {
-        type: Object,
-        default() {
-          return {};
-        }
-      },
-      filterable: Boolean,
-      props: {
-        type: Object,
-        default() {
-          return {
-            label: 'label',
-            key: 'key',
-            disabled: 'disabled'
-          };
-        }
+  props: {
+    data: {
+      type: Array,
+      default() {
+        return [];
       }
     },
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    buttonTexts: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    filterPlaceholder: {
+      type: String,
+      default: ''
+    },
+    filterMethod: Function,
+    leftDefaultChecked: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    rightDefaultChecked: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    renderContent: Function,
+    value: {
+      type: Array,
+      default() {
+        return [];
+      }
+    },
+    format: {
+      type: Object,
+      default() {
+        return {};
+      }
+    },
+    filterable: Boolean,
+    props: {
+      type: Object,
+      default() {
+        return {
+          label: 'label',
+          key: 'key',
+          disabled: 'disabled'
+        };
+      }
+    }
+  },
 
-    data() {
+  data() {
+    return {
+      leftChecked: [],
+      rightChecked: []
+    };
+  },
+
+  computed: {
+    dataObj() {
+      const key = this.props.key;
+      return this.data.reduce((o, cur) => (o[cur[key]] = cur) && o, {});
+    },
+
+    sourceData() {
+      return this.data.filter(
+        item => this.value.indexOf(item[this.props.key]) === -1
+      );
+    },
+
+    targetData() {
+      return this.value.map(key => this.dataObj[key]);
+    },
+
+    hasButtonTexts() {
+      return this.buttonTexts.length === 2;
+    }
+  },
+
+  watch: {
+    value(val) {
+      this.dispatch('ElFormItem', 'el.form.change', val);
+    }
+  },
+
+  methods: {
+    getMigratingConfig() {
       return {
-        leftChecked: [],
-        rightChecked: []
+        props: {
+          'footer-format': 'footer-format is renamed to format.'
+        }
       };
     },
 
-    computed: {
-      sourceData() {
-        return this.data.filter(item => this.value.indexOf(item[this.props.key]) === -1);
-      },
-
-      targetData() {
-        return this.data.filter(item => this.value.indexOf(item[this.props.key]) > -1);
-      },
-
-      hasButtonTexts() {
-        return this.buttonTexts.length === 2;
-      }
+    onSourceCheckedChange(val) {
+      this.leftChecked = val;
     },
 
-    watch: {
-      value(val) {
-        this.dispatch('ElFormItem', 'el.form.change', val);
-      }
+    onTargetCheckedChange(val) {
+      this.rightChecked = val;
     },
 
-    methods: {
-      getMigratingConfig() {
-        return {
-          props: {
-            'footer-format': 'footer-format is renamed to format.'
-          }
-        };
-      },
-
-      onSourceCheckedChange(val) {
-        this.leftChecked = val;
-      },
-
-      onTargetCheckedChange(val) {
-        this.rightChecked = val;
-      },
-
-      addToLeft() {
-        let currentValue = this.value.slice();
-        this.rightChecked.forEach(item => {
-          const index = currentValue.indexOf(item);
-          if (index > -1) {
-            currentValue.splice(index, 1);
-          }
-        });
-        this.$emit('input', currentValue);
-        this.$emit('change', currentValue, 'left', this.rightChecked);
-      },
-
-      addToRight() {
-        let currentValue = this.value.slice();
-        this.leftChecked.forEach(item => {
-          if (this.value.indexOf(item) === -1) {
-            currentValue = currentValue.concat(item);
-          }
-        });
-        this.$emit('input', currentValue);
-        this.$emit('change', currentValue, 'right', this.leftChecked);
-      },
-
-      clearQuery(which) {
-        if (which === 'left') {
-          this.$refs.leftPanel.query = '';
-        } else if (which === 'right') {
-          this.$refs.rightPanel.query = '';
+    addToLeft() {
+      let currentValue = this.value.slice();
+      this.rightChecked.forEach(item => {
+        const index = currentValue.indexOf(item);
+        if (index > -1) {
+          currentValue.splice(index, 1);
         }
+      });
+      this.$emit('input', currentValue);
+      this.$emit('change', currentValue, 'left', this.rightChecked);
+    },
+
+    addToRight() {
+      let currentValue = this.value.slice();
+      this.leftChecked.forEach(item => {
+        if (this.value.indexOf(item) === -1) {
+          currentValue = [item].concat(currentValue);
+        }
+      });
+      this.$emit('input', currentValue);
+      this.$emit('change', currentValue, 'right', this.leftChecked);
+    },
+
+    clearQuery(which) {
+      if (which === 'left') {
+        this.$refs.leftPanel.query = '';
+      } else if (which === 'right') {
+        this.$refs.rightPanel.query = '';
       }
     }
-  };
+  }
+};
 </script>
