e8932c05a8532f0eb568a664a6468a29f97ad941
a.konuhov <alexandrkonuhov@gmail.com>
2018-03-03 21:15:24 +0300

    delete file

--- packages/checkbox-list/index.js
+++
@@ -1,8 +0,0 @@
-import TmCheckboxList from '../checkbox/src/checkbox-group.vue';
-
-/* istanbul ignore next */
-TmCheckboxList.install = function(Vue) {
-  Vue.component(TmCheckboxList.name, TmCheckboxList);
-};
-
-export default TmCheckboxList;
