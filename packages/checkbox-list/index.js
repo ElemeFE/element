cb421a2960e8af57d3aaee3405300b11310000cb
a.konuhov <alexandrkonuhov@gmail.com>
2018-03-03 21:11:07 +0300

    Add new file [NEF-48]

---
+++ packages/checkbox-list/index.js
@@ -0,0 +1,8 @@
+import TmCheckboxList from '../checkbox/src/checkbox-group.vue';
+
+/* istanbul ignore next */
+TmCheckboxList.install = function(Vue) {
+  Vue.component(TmCheckboxList.name, TmCheckboxList);
+};
+
+export default TmCheckboxList;
