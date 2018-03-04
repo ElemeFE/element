657f9b9c3a48408c2d0d1688b3f9cae0a01d60c7
杨奕 <Leopoldthecoder@users.noreply.github.com>
2018-03-04 11:16:06 +0800

    Popover & Tooltip: focus reference element on tab if it's focusable (#9990)

--- packages/tooltip/src/main.js
+++ packages/tooltip/src/main.js
@@ -117,7 +117,18 @@ export default {
       this.$el.setAttribute('tabindex', 0);
       on(this.referenceElm, 'mouseenter', this.show);
       on(this.referenceElm, 'mouseleave', this.hide);
-      on(this.referenceElm, 'focus', this.handleFocus);
+      on(this.referenceElm, 'focus', () => {
+        if (!this.$slots.default || !this.$slots.default.length) {
+          this.handleFocus();
+          return;
+        }
+        const instance = this.$slots.default[0].componentInstance;
+        if (instance && instance.focus) {
+          instance.focus();
+        } else {
+          this.handleFocus();
+        }
+      });
       on(this.referenceElm, 'blur', this.handleBlur);
       on(this.referenceElm, 'click', this.removeFocusing);
     }
