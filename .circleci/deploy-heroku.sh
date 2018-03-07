a97353991d7dcfb4297d19e497700c5202d03886
roquie <roquie0@gmail.com>
2018-03-07 20:58:49 +0300

    Fix the old docker login parameters.

--- .circleci/deploy-heroku.sh
+++ .circleci/deploy-heroku.sh
@@ -2,5 +2,5 @@
 
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
-docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
