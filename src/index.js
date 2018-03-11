d530df3f43b8e1d4d14e613b7df00d09de756cdb
Nikita Brylev <nbrylevv@gmail.com>
2018-03-12 00:02:12 +0300

    run dist

--- src/index.js
+++ src/index.js
@@ -57,10 +57,10 @@ import Main from '../packages/main/index.js';
 import Footer from '../packages/footer/index.js';
 import Badge from '../packages/badge/index.js';
 import TimeInfo from '../packages/time-info/index.js';
+import EntityCounter from '../packages/entity-counter/index.js';
 import PriceInfo from '../packages/price-info/index.js';
 import Sidebar from '../packages/sidebar/index.js';
 import NavBar from '../packages/nav-bar/index.js';
-import EntityCounter from '../packages/entity-counter/index.js';
 import locale from 'tmconsulting-ui/src/locale';
 import CollapseTransition from 'tmconsulting-ui/src/transitions/collapse-transition';
 
@@ -121,10 +121,10 @@ const components = [
   Footer,
   Badge,
   TimeInfo,
-  PriceInfo,
-  NavBar,
   EntityCounter,
+  PriceInfo,
   Sidebar,
+  NavBar,
   CollapseTransition
 ];
 
@@ -220,10 +220,10 @@ module.exports = {
   Footer,
   Badge,
   TimeInfo,
+  EntityCounter,
   PriceInfo,
   Sidebar,
-  NavBar,
-  EntityCounter
+  NavBar
 };
 
 module.exports.default = module.exports;
