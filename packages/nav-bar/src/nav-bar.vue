cd39734bfb2ef5c5cc1beda9625c3417b1789faa
Nikita Brylev <nbrylevv@gmail.com>
2018-03-11 16:42:58 +0300

    navBar fix

--- packages/nav-bar/src/nav-bar.vue
+++ packages/nav-bar/src/nav-bar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tm-nav-bar">
     <div class="tm-nav-bar__left">
-      <div class="tm-nav-bar__title">{{ title }}</div>
+      <div class="tm-nav-bar__title" v-if="title">{{ title }}</div>
       <slot name="left"></slot>
     </div>
     <div class="tm-nav-bar__right">
@@ -11,9 +11,15 @@
 </template>
 <script>
   export default {
-
     name: 'TmNavBar',
 
-    componentName: 'TmNavBar'
+    componentName: 'TmNavBar',
+
+    props: {
+      title: {
+        type: String,
+        default: ''
+      }
+    }
   };
 </script>
