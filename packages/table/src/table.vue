a7908fdc16c2c31b6760727c401dd22ac5896186
Furybean <long.zhang@ele.me>
2018-03-12 18:30:45 +0800

    fix fixed column style on safari

--- packages/table/src/table.vue
+++ packages/table/src/table.vue
@@ -99,7 +99,7 @@
           :border="border"
           :store="store"
           :style="{
-            width: layout.fixedWidth ? layout.fixedWidth + 'px' : ''
+            width: layout.bodyWidth ? layout.bodyWidth + 'px' : ''
           }"></table-header>
       </div>
       <div
@@ -117,7 +117,7 @@
           :row-class-name="rowClassName"
           :row-style="rowStyle"
           :style="{
-            width: layout.fixedWidth ? layout.fixedWidth + 'px' : ''
+            width: layout.bodyWidth ? layout.bodyWidth + 'px' : ''
           }">
         </table-body>
         <div
@@ -139,7 +139,7 @@
           :summary-method="summaryMethod"
           :store="store"
           :style="{
-            width: layout.fixedWidth ? layout.fixedWidth + 'px' : ''
+            width: layout.bodyWidth ? layout.bodyWidth + 'px' : ''
           }"></table-footer>
       </div>
     </div>
@@ -162,7 +162,7 @@
           :border="border"
           :store="store"
           :style="{
-            width: layout.rightFixedWidth ? layout.rightFixedWidth + 'px' : ''
+            width: layout.bodyWidth ? layout.bodyWidth + 'px' : ''
           }"></table-header>
       </div>
       <div
@@ -180,7 +180,7 @@
           :row-style="rowStyle"
           :highlight="highlightCurrentRow"
           :style="{
-            width: layout.rightFixedWidth ? layout.rightFixedWidth + 'px' : ''
+            width: layout.bodyWidth ? layout.bodyWidth + 'px' : ''
           }">
         </table-body>
       </div>
@@ -196,7 +196,7 @@
           :summary-method="summaryMethod"
           :store="store"
           :style="{
-            width: layout.rightFixedWidth ? layout.rightFixedWidth + 'px' : ''
+            width: layout.bodyWidth ? layout.bodyWidth + 'px' : ''
           }"></table-footer>
       </div>
     </div>
