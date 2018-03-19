54b3a65fcf8e063517a053bad9354daa7af77bad
a.konuhov <alexandrkonuhov@gmail.com>
2018-03-19 13:22:53 +0300

    Add file [NEF-76]

---
+++ packages/form-group-item/index.js
@@ -0,0 +1,8 @@
+import TmFormGroupItem from '../form/src/form-group-item';
+
+/* istanbul ignore next */
+TmFormGroupItem.install = function(Vue) {
+  Vue.component(TmFormGroupItem.name, TmFormGroupItem);
+};
+
+export default TmFormGroupItem;
