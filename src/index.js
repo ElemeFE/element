d0914ba55e60b8d25a2a05ad7270684b3ece45cc
Nikita Brylev <nbrylevv@gmail.com>
2018-03-25 18:25:36 +0300

    update

--- src/index.js
+++ src/index.js
@@ -160,7 +160,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 module.exports = {
-  version: '1.1.0-revert',
+  version: '1.1.1',
   locale: locale.use,
   i18n: locale.i18n,
   install,
