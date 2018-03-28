248e370683b74c6f9142e807f821bc25bb5005dd
Nikita Brylev <nbrylevv@gmail.com>
2018-03-28 15:48:57 +0300

    return: fixed for web build

--- packages/popover/src/directive.js
+++ packages/popover/src/directive.js
@@ -1,12 +1,7 @@
 export default {
-  bind: function(el, binding, vnode) {
+  bind(el, binding, vnode) {
     // vue中 v-popover:argument 和 v-popover="variate|| expression"得到的binding数据是不同的。后者可以指向动态popver组件，可以极大的增强popover指令的灵活程度。
-    var _ref;
-    if (binding.expression) {
-      _ref = binding.value;
-    } else {
-      _ref = binding.arg;
-    }
+    const _ref = binding.expression ? binding.value : binding.arg;
     vnode.context.$refs[_ref].$refs.reference = el;
   }
 };
