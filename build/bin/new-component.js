93d9dd638908c16308e45fea57c42ad861acdcd3
Nikita Brylev <nbrylevv@gmail.com>
2018-03-31 15:52:18 +0300

    Added command to create new components

---
+++ build/bin/new-component.js
@@ -0,0 +1,118 @@
+'use strict';
+
+console.log();
+process.on('exit', () => {
+  console.log();
+});
+
+if (!process.argv[2]) {
+  console.error('Please enter new component name');
+  process.exit(1);
+}
+
+const path = require('path');
+const fs = require('fs');
+const fileSave = require('file-save');
+const uppercamelcase = require('uppercamelcase');
+const componentname = process.argv[2];
+const ComponentName = uppercamelcase(componentname);
+const PackagePath = path.resolve(__dirname, '../../packages', componentname);
+const Files = [
+  {
+    filename: 'index.js',
+    content: `import ${ComponentName} from './src/main';
+
+/* istanbul ignore next */
+${ComponentName}.install = function(Vue) {
+  Vue.component(${ComponentName}.name, ${ComponentName});
+};
+
+export default ${ComponentName};`
+  },
+  {
+    filename: 'src/main.vue',
+    content: `<template>
+  <div class="tm-${componentname}"></div>
+</template>
+
+<script>
+export default {
+  name: 'Tm${ComponentName}'
+};
+</script>`
+  },
+  {
+    filename: '../theme-chalk/src/' + componentname + '.scss',
+    content: `@import "common/var";
+@import "mixins/mixins";
+
+@include b(${componentname}) {
+
+}`
+  },
+  {
+    filename: path.join('../../examples/docs/en-US', `${componentname}.md`),
+    content: `## ${ComponentName}`
+  },
+  {
+    filename: path.join('../../test/unit/specs', `${componentname}.spec.js`),
+    content: `import { createTest, destroyVM } from '../util';
+import ${ComponentName} from 'packages/${componentname}';
+
+describe('${ComponentName}', () => {
+  let vm;
+  afterEach(() => {
+    destroyVM(vm);
+  });
+
+  it('create', () => {
+    vm = createTest(${ComponentName}, true);
+    expect(vm.$el).to.exist;
+  });
+});
+`
+  }
+];
+
+// components.json
+const componentsFile = require('../../components.json');
+if (componentsFile[componentname]) {
+  console.error(`${componentname} already added.`);
+  process.exit(1);
+}
+componentsFile[componentname] = `./packages/${componentname}/index.js`;
+fileSave(path.join(__dirname, '../../components.json'))
+  .write(JSON.stringify(componentsFile, null, '  '), 'utf8')
+  .end('\n');
+
+// Add package dir
+Files.forEach(file => {
+  console.log(path.join(PackagePath, file.filename))
+  fileSave(path.join(PackagePath, file.filename))
+    .write(file.content, 'utf8')
+    .end('\n');
+});
+
+// Import styles to index.scss
+const stylesFile = path.join(__dirname, '../../packages/theme-chalk/src/index.scss')
+const stylesData = fs.readFileSync(stylesFile, 'utf8')
+fileSave(stylesFile)
+  .write(stylesData)
+  .end('@import "./' + componentname + '.scss";\n')
+
+// nav.config.json
+const navConfigFile = require('../../examples/nav.config.json');
+
+Object.keys(navConfigFile).forEach(lang => {
+  let groups = navConfigFile[lang][2].groups;
+  groups[groups.length - 1].list.push({
+    path: `/${componentname}`,
+    title: ComponentName
+  });
+});
+
+fileSave(path.join(__dirname, '../../examples/nav.config.json'))
+  .write(JSON.stringify(navConfigFile, null, '  '), 'utf8')
+  .end('\n');
+
+console.log('DONE!');
