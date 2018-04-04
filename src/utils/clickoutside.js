ae4a8cd7c09aaf4ed9451f0726dcf17d65bafe87
TaoXuSheng <xusheng.tao@ele.me>
2018-04-04 15:51:24 +0800

    修复点击 iframe 无法触发事件问题。

--- src/utils/clickoutside.js
+++ src/utils/clickoutside.js
@@ -1,40 +1,7 @@
-import Vue from 'vue';
-import { on } from 'element-ui/src/utils/dom';
-
-const nodeList = [];
-const ctx = '@@clickoutsideContext';
-
-let startClick;
-let seed = 0;
-
-!Vue.prototype.$isServer && on(document, 'mousedown', e => (startClick = e));
-
-!Vue.prototype.$isServer && on(document, 'mouseup', e => {
-  nodeList.forEach(node => node[ctx].documentHandler(e, startClick));
-});
-
-function createDocumentHandler(el, binding, vnode) {
-  return function(mouseup = {}, mousedown = {}) {
-    if (!vnode ||
-      !vnode.context ||
-      !mouseup.target ||
-      !mousedown.target ||
-      el.contains(mouseup.target) ||
-      el.contains(mousedown.target) ||
-      el === mouseup.target ||
-      (vnode.context.popperElm &&
-      (vnode.context.popperElm.contains(mouseup.target) ||
-      vnode.context.popperElm.contains(mousedown.target)))) return;
-
-    if (binding.expression &&
-      el[ctx].methodName &&
-      vnode.context[el[ctx].methodName]) {
-      vnode.context[el[ctx].methodName]();
-    } else {
-      el[ctx].bindingFn && el[ctx].bindingFn();
-    }
-  };
-}
+import {
+  bind as foucsBind,
+  unbind as focusUnbind
+} from 'focus-outside';
 
 /**
  * v-clickoutside
@@ -45,32 +12,17 @@ function createDocumentHandler(el, binding, vnode) {
  * ```
  */
 export default {
-  bind(el, binding, vnode) {
-    nodeList.push(el);
-    const id = seed++;
-    el[ctx] = {
-      id,
-      documentHandler: createDocumentHandler(el, binding, vnode),
-      methodName: binding.expression,
-      bindingFn: binding.value
-    };
+  bind(el, binding) {
+    foucsBind(el, binding.value);
   },
 
-  update(el, binding, vnode) {
-    el[ctx].documentHandler = createDocumentHandler(el, binding, vnode);
-    el[ctx].methodName = binding.expression;
-    el[ctx].bindingFn = binding.value;
+  unbind(el, binding) {
+    focusUnbind(el, binding.value);
   },
 
-  unbind(el) {
-    let len = nodeList.length;
-
-    for (let i = 0; i < len; i++) {
-      if (nodeList[i][ctx].id === el[ctx].id) {
-        nodeList.splice(i, 1);
-        break;
-      }
-    }
-    delete el[ctx];
+  update(el, { value, oldValue }) {
+    if (value === oldValue) return;
+    focusUnbind(el, oldValue);
+    foucsBind(el, value);
   }
 };
