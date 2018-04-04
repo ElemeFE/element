eaf60d05346035ccdf16f9147d5814f3128794c2
TaoXuSheng <xusheng.tao@ele.me>
2018-04-04 15:24:08 +0800

    修复 clickoutside 点击 iframe 区域无法触发问题。

--- src/utils/clickoutside.js
+++ src/utils/clickoutside.js
@@ -1,16 +1,8 @@
-import Vue from 'vue';
-import { on } from 'element-ui/src/utils/dom';
+import {
+  bind as foucsBind,
+  unbind as focusUnbind
+} from 'focus-outside';
 
-const nodeList = [];
-const ctx = '@@clickoutsideContext';
-
-let startClick;
-
-!Vue.prototype.$isServer && on(document, 'mousedown', e => (startClick = e));
-
-!Vue.prototype.$isServer && on(document, 'mouseup', e => {
-  nodeList.forEach(node => node[ctx].documentHandler(e, startClick));
-});
 /**
  * v-clickoutside
  * @desc 点击元素外面才会触发的事件
@@ -20,44 +12,17 @@ let startClick;
  * ```
  */
 export default {
-  bind(el, binding, vnode) {
-    const id = nodeList.push(el) - 1;
-    const documentHandler = function(mouseup, mousedown) {
-      if (!vnode.context ||
-        el.contains(mouseup.target) ||
-        (vnode.context.popperElm &&
-        (vnode.context.popperElm.contains(mouseup.target) ||
-        vnode.context.popperElm.contains(mousedown.target)))) return;
-
-      if (binding.expression &&
-        el[ctx].methodName &&
-        vnode.context[el[ctx].methodName]) {
-        vnode.context[el[ctx].methodName]();
-      } else {
-        el[ctx].bindingFn && el[ctx].bindingFn();
-      }
-    };
-    el[ctx] = {
-      id,
-      documentHandler,
-      methodName: binding.expression,
-      bindingFn: binding.value
-    };
+  bind(el, binding) {
+    foucsBind(el, binding.value);
   },
 
-  update(el, binding) {
-    el[ctx].methodName = binding.expression;
-    el[ctx].bindingFn = binding.value;
+  unbind(el, binding) {
+    focusUnbind(el, binding.value);
   },
 
-  unbind(el) {
-    let len = nodeList.length;
-
-    for (let i = 0; i < len; i++) {
-      if (nodeList[i][ctx].id === el[ctx].id) {
-        nodeList.splice(i, 1);
-        break;
-      }
-    }
+  update(el, { value, oldValue }) {
+    if (value === oldValue) return;
+    focusUnbind(el, oldValue);
+    foucsBind(el, value);
   }
 };
