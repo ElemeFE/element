8b379a57df35651244a2ec180b3dd3b743e83c1a
Jason Zoo <zjczoo@aliyun.com>
2018-04-25 17:34:00 +0800

    Switch: use CSS to process button position (#10879)
    * switch transformx value not flexiable
    
    * add -1px for button

--- packages/switch/src/component.vue
+++ packages/switch/src/component.vue
@@ -26,7 +26,6 @@
       <span v-if="!inactiveIconClass && inactiveText" :aria-hidden="checked">{{ inactiveText }}</span>
     </span>
     <span class="el-switch__core" ref="core" :style="{ 'width': coreWidth + 'px' }">
-      <span class="el-switch__button" :style="{ transform }"></span>
     </span>
     <span
       :class="['el-switch__label', 'el-switch__label--right', checked ? 'is-active' : '']"
@@ -106,9 +105,6 @@
     computed: {
       checked() {
         return this.value === this.activeValue;
-      },
-      transform() {
-        return this.checked ? `translate3d(${ this.coreWidth - 20 }px, 0, 0)` : '';
       },
       switchDisabled() {
         return this.disabled || (this.elForm || {}).disabled;
