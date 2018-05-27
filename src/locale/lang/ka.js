e5517f8dcec22263ddce6a0321042d17db777e2f
tskapo <gamotserebi@icloud.com>
2018-05-27 10:32:20 +0200

    Create ka.js
    Georgian locale

---
+++ src/locale/lang/ka.js
@@ -0,0 +1,127 @@
+(function(global, factory) {
+	if (typeof define === "function" && define.amd) {
+		define("element/locale/fr", ["module", "exports"], factory);
+	} else if (typeof exports !== "undefined") {
+		factory(module, exports);
+	} else {
+		var mod = {
+			exports: {}
+		};
+		factory(mod, mod.exports);
+		global.ELEMENT.lang = global.ELEMENT.lang || {};
+		global.ELEMENT.lang.ka = mod.exports;
+	}
+})(this, function(module, exports) {
+	"use strict";
+
+	exports.__esModule = true;
+	exports.default = {
+		el: {
+			colorpicker: {
+				confirm: "კეთილი",
+				clear: "წაშლა"
+			},
+			datepicker: {
+				now: "ახლა",
+				today: "დღეს.",
+				cancel: "გაუქმება",
+				clear: "გასუფთავება",
+				confirm: "კეთილი",
+				selectDate: "თარიღის არჩევა",
+				selectTime: "დროის არჩევა",
+				startDate: "დაწყების თარიღი",
+				startTime: "დაწყების დრო",
+				endDate: "დამთავრების თარიღი",
+				endTime: "დამთავრების დრო",
+				prevYear: "შარშან",
+				nextYear: "გაისად",
+				prevMonth: "წინა თვე",
+				nextMonth: "შემდეგი თვე",
+				year: "წელი",
+				month1: "იანვარი",
+				month2: "თებერვალი",
+				month3: "მარტი",
+				month4: "აპრილი",
+				month5: "მაისი",
+				month6: "ივნისი",
+				month7: "ივლისი",
+				month8: "აგვისტო",
+				month9: "სექტემბერი",
+				month10: "ოქტომბერი",
+				month11: "ნოემბერი",
+				month12: "დეკემბერი",
+				// week: 'კვირა',
+				weeks: {
+					sun: "კვ",
+					mon: "ორშ",
+					tue: "სამ",
+					wed: "ოთხ",
+					thu: "ხუთ",
+					fri: "პარ",
+					sat: "შაბ"
+				},
+				months: {
+					jan: "იან",
+					feb: "თებ",
+					mar: "მარ",
+					apr: "აპრ",
+					may: "მაი",
+					jun: "ივნ",
+					jul: "ივლ",
+					aug: "აგვ",
+					sep: "სექ",
+					oct: "ოქტ",
+					nov: "ნოე",
+					dec: "დეკ"
+				}
+			},
+			select: {
+				loading: "იტვირთება",
+				noMatch: "შესატყვისობა ვერ მოიძებნა",
+				noData: "მონაცემები ვერ მოიძებნა",
+				placeholder: "ამორჩევა"
+			},
+			cascader: {
+				noMatch: "შესატყვისობა ვერ მოიძებნა",
+				loading: "იტვირთება",
+				placeholder: "ამორჩევა"
+			},
+			pagination: {
+				goto: "გადასვლა",
+				pagesize: "/გვერდი",
+				total: "სულ {total}",
+				pageClassifier: ""
+			},
+			messagebox: {
+				confirm: "დადასტურება",
+				cancel: "გაუქმება",
+				error: "შეცდომა"
+			},
+			upload: {
+				deleteTip: "ფაილის მოსაცილებლად დაწკაპეთ „წაშლა“-ზე",
+				delete: "წაშლა",
+				preview: "თვალის შევლება",
+				continue: "გაგრძელება"
+			},
+			table: {
+				emptyText: "მონაცემები ვერ მოიძებნა",
+				confirmFilter: "დადასტურება",
+				resetFilter: "რეინიციალიზაცია",
+				clearFilter: "ყველა",
+				sumText: "ჯამი"
+			},
+			tree: {
+				emptyText: "მონაცემები ვერ მოიძებნა"
+			},
+			transfer: {
+				noMatch: "შესატყვისობა ვერ მოიძებნა",
+				noData: "მონაცემები ვერ მოიძებნა",
+				titles: ["სია 1", "სია 2"],
+				filterPlaceholder: "შემოიყვანეთ საკვანძო სიტყვა",
+				noCheckedFormat: "{total} ოდენობა",
+				hasCheckedFormat: "მონიშნულია {checked}/{total}"
+			}
+		}
+	};
+	module.exports = exports["default"];
+});
