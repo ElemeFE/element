7328f312902e5b155e35a86b0d435ba7a9874f17
zhiyang <zhiyang.deng@ele.me>
2018-06-05 15:28:09 +0800

    Dialog: trigger closed event when close animation ends

--- packages/dialog/src/component.vue
+++ packages/dialog/src/component.vue
@@ -1,5 +1,7 @@
 <template>
-  <transition name="dialog-fade">
+  <transition
+    name="dialog-fade"
+    @after-leave="afterLeave">
     <div class="el-dialog__wrapper" v-show="visible" @click.self="handleWrapperClick">
       <div
         class="el-dialog"
@@ -166,6 +168,9 @@
       updatePopper() {
         this.broadcast('ElSelectDropdown', 'updatePopper');
         this.broadcast('ElDropdownMenu', 'updatePopper');
+      },
+      afterLeave() {
+        this.$emit('closed');
       }
     },
 
