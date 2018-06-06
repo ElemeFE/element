cbb0fce62e9c906ac4224c477338a16874f5a574
akifo <akiho@bb07.net>
2018-06-06 16:51:44 +0900

    Popover: Fix set reference in loop

--- packages/popover/src/directive.js
+++ packages/popover/src/directive.js
@@ -1,8 +1,11 @@
 const getReference = (el, binding, vnode) => {
   const _ref = binding.expression ? binding.value : binding.arg;
   const popper = vnode.context.$refs[_ref];
-  if (popper) {
+  const isArray = Array.isArray(popper);
+  if (popper && !isArray) {
     popper.$refs.reference = el;
+  } else if (isArray) {
+    popper[0].$refs.reference = el;
   }
 };
 
