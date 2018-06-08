43e9b1a83683eaadaf0b60ecc8f729e198b7bc45
hetech <dengzhiyang123@126.com>
2018-06-08 17:44:13 +0800

    Update directive.js

--- packages/popover/src/directive.js
+++ packages/popover/src/directive.js
@@ -1,11 +1,12 @@
 const getReference = (el, binding, vnode) => {
   const _ref = binding.expression ? binding.value : binding.arg;
   const popper = vnode.context.$refs[_ref];
-  const isArray = Array.isArray(popper);
-  if (popper && !isArray) {
-    popper.$refs.reference = el;
-  } else if (isArray) {
-    popper[0].$refs.reference = el;
+  if (popper) {
+    if (Array.isArray(popper)) {
+      popper[0].$refs.reference = el;
+    } else {
+      popper.$refs.reference = el;
+    }
   }
 };
 
