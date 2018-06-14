dd3bb2ce20830c6e6984f12fff385dc30cfe54a8
杨奕 <Leopoldthecoder@users.noreply.github.com>
2018-06-14 11:20:29 +0800

    Pagination: add size to nested select (#11622)

--- packages/pagination/src/pagination.js
+++ packages/pagination/src/pagination.js
@@ -179,6 +179,7 @@ export default {
             <el-select
               value={ this.$parent.internalPageSize }
               popperClass={ this.$parent.popperClass || '' }
+              size="mini"
               on-input={ this.handleChange }
               disabled={ this.$parent.disabled }>
               {
