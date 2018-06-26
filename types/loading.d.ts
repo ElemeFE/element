4e0dd86cf6013247d47300564d35162a5b739942
Leopoldthecoder <Leopoldthecuber@gmail.com>
2018-06-26 11:18:33 +0800

    Loading: add missing definition of directive

--- types/loading.d.ts
+++ types/loading.d.ts
@@ -50,6 +50,8 @@ export interface ElLoading {
 
   /** If you do not have a specific DOM node to attach the Loading directive, or if you simply prefer not to use Loading as a directive, you can call this service with some configs to open a Loading instance. */
   service (options: LoadingServiceOptions): ElLoadingComponent
+
+  directive: object
 }
 
 declare module 'vue/types/vue' {
