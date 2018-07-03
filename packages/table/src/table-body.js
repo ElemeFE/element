e9b988b419d43d15c30ab5f90197cd5dc3bda4de
Leopoldthecoder <Leopoldthecuber@gmail.com>
2018-07-03 14:47:28 +0800

    Table: setCurrentRow only when highlight-current-row is true

--- packages/table/src/table-body.js
+++ packages/table/src/table-body.js
@@ -392,7 +392,7 @@ export default {
     },
 
     handleClick(event, row) {
-      this.store.commit('setCurrentRow', row);
+      this.table.highlightCurrentRow && this.store.commit('setCurrentRow', row);
       this.handleEvent(event, row, 'click');
     },
 
