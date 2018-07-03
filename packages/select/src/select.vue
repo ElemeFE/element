ef74ee05b3cf14391f8cd6b2418481dfbe0b5a74
杨奕 <Leopoldthecoder@users.noreply.github.com>
2018-07-03 15:03:50 +0800

    Select: revert #10105 (#11837)

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -851,9 +851,6 @@
 
       this.$on('handleOptionClick', this.handleOptionSelect);
       this.$on('setSelected', this.setSelected);
-      this.$on('fieldReset', () => {
-        this.dispatch('ElFormItem', 'el.form.change');
-      });
     },
 
     mounted() {
