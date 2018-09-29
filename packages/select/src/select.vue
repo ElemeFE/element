98f35de12f8d9cdd9399577a86094a798526bd9a
panjiandong <40926597+panjiandong@users.noreply.github.com>
2018-09-29 11:31:58 +0800

    TestUpdate

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="el-select"
+    class="el-select test"
     :class="[selectSize ? 'el-select--' + selectSize : '']"
     @click.stop="toggleMenu"
     v-clickoutside="handleClose">
