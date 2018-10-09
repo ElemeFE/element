71305bc7e37da8646c546bf0be41788df59f4d11
leonardo <leonardotessaroalves@gmail.com>
2018-10-08 21:36:30 -0300

    extract scss varsand build json iconlist

--- build/bin/iconInit.js
+++ build/bin/iconInit.js
@@ -1,6 +1,24 @@
 'use strict';
 
-var postcss = require('postcss');
+const sassExtract = require('sass-extract');
+const fs = require('fs');
+const path = require('path');
+const fontFile = path.resolve(__dirname, '../../packages/theme-chalk/src/iconList.scss')
+
+
+sassExtract.render({
+  file: fontFile
+})
+.then(rendered => {
+  let iconArray = []
+  let icons = rendered.vars.global.$siagriIcons.value;
+  Object.keys(icons).forEach((icon) => {
+    iconArray.push(icon)
+  })
+  fs.writeFile(path.resolve(__dirname, '../../examples/icon.json'), JSON.stringify(iconArray), () => {});
+})
+
+/* var postcss = require('postcss');
 var fs = require('fs');
 var path = require('path');
 var fontFile = fs.readFileSync(path.resolve(__dirname, '../../packages/theme-chalk/src/icon.scss'), 'utf8');
@@ -15,6 +33,6 @@ nodes.forEach((node) => {
   if (arr && arr[1]) {
     classList.push(arr[1]);
   }
-});
+}); */
 
-fs.writeFile(path.resolve(__dirname, '../../examples/icon.json'), JSON.stringify(classList), () => {});
+// fs.writeFile(path.resolve(__dirname, '../../examples/icon.json'), JSON.stringify(classList), () => {});
