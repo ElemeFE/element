3bdd1c203db738028654af1b99264c5bc061896f
leonardo <leonardotessaroalves@gmail.com>
2018-10-23 11:19:10 -0300

    change form filds to fildList
    its a workaround to integrate vee-valite as the default validation tool. As soon as possible we should migrate this feature into element-ui

--- packages/form/src/form.vue
+++ packages/form/src/form.vue
@@ -56,19 +56,19 @@
     },
     data() {
       return {
-        fields: []
+        fieldList: []
       };
     },
     created() {
       this.$on('el.form.addField', (field) => {
         if (field) {
-          this.fields.push(field);
+          this.fieldList.push(field);
         }
       });
       /* istanbul ignore next */
       this.$on('el.form.removeField', (field) => {
         if (field.prop) {
-          this.fields.splice(this.fields.indexOf(field), 1);
+          this.fieldList.splice(this.fieldList.indexOf(field), 1);
         }
       });
     },
@@ -79,14 +79,14 @@
           console.warn('[Element Warn][Form]model is required for resetFields to work.');
           return;
         }
-        this.fields.forEach(field => {
+        this.fieldList.forEach(field => {
           field.resetField();
         });
       },
       clearValidate(props = []) {
         const fields = props.length
-          ? this.fields.filter(field => props.indexOf(field.prop) > -1)
-          : this.fields;
+          ? this.fieldList.filter(field => props.indexOf(field.prop) > -1)
+          : this.fieldList;
         fields.forEach(field => {
           field.clearValidate();
         });
@@ -110,17 +110,17 @@
         let valid = true;
         let count = 0;
         // 如果需要验证的fields为空，调用验证时立刻返回callback
-        if (this.fields.length === 0 && callback) {
+        if (this.fieldList.length === 0 && callback) {
           callback(true);
         }
         let invalidFields = {};
-        this.fields.forEach(field => {
+        this.fieldList.forEach(field => {
           field.validate('', (message, field) => {
             if (message) {
               valid = false;
             }
             invalidFields = objectAssign({}, invalidFields, field);
-            if (typeof callback === 'function' && ++count === this.fields.length) {
+            if (typeof callback === 'function' && ++count === this.fieldList.length) {
               callback(valid, invalidFields);
             }
           });
@@ -131,7 +131,7 @@
         }
       },
       validateField(prop, cb) {
-        let field = this.fields.filter(field => field.prop === prop)[0];
+        let field = this.fieldList.filter(field => field.prop === prop)[0];
         if (!field) { throw new Error('must call validateField with valid prop string!'); }
 
         field.validate('', cb);
