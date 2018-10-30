1fe641832e5670170eba3ce3fa7ac5d8933ed8d4
Kermo <kermo@vedru.eu>
2018-10-30 10:58:03 +0200

    Add missing Finnish translations

--- src/locale/lang/fi.js
+++ src/locale/lang/fi.js
@@ -16,10 +16,10 @@ export default {
       startTime: 'Aloitusaika',
       endDate: 'Lopetuspäivä',
       endTime: 'Lopetusaika',
-      prevYear: 'Previous Year', // to be translated
-      nextYear: 'Next Year', // to be translated
-      prevMonth: 'Previous Month', // to be translated
-      nextMonth: 'Next Month', // to be translated
+      prevYear: 'Edellinen Vuosi',
+      nextYear: 'Seuraava Vuosi',
+      prevMonth: 'Edellinen Kuukausi',
+      nextMonth: 'Seuraava Kuukausi',
       year: '',
       month1: 'tammikuu',
       month2: 'helmikuu',
@@ -82,7 +82,7 @@ export default {
       error: 'Virheellinen syöte'
     },
     upload: {
-      deleteTip: 'press delete to remove', // to be translated
+      deleteTip: 'Lehdistö Poista että poistaa',
       delete: 'Poista',
       preview: 'Esikatsele',
       continue: 'Jatka'
@@ -92,7 +92,7 @@ export default {
       confirmFilter: 'Vahvista',
       resetFilter: 'Tyhjennä',
       clearFilter: 'Kaikki',
-      sumText: 'Sum' // to be translated
+      sumText: 'Summa'
     },
     tree: {
       emptyText: 'Ei tietoja'
@@ -100,10 +100,10 @@ export default {
     transfer: {
       noMatch: 'Ei vastaavia tietoja',
       noData: 'Ei tietoja',
-      titles: ['List 1', 'List 2'], // to be translated
-      filterPlaceholder: 'Enter keyword', // to be translated
-      noCheckedFormat: '{total} items', // to be translated
-      hasCheckedFormat: '{checked}/{total} checked' // to be translated
+      titles: ['Luettelo 1', 'Luettelo 2'],
+      filterPlaceholder: 'Kirjoita avainsana',
+      noCheckedFormat: '{total} kohdetta',
+      hasCheckedFormat: '{checked}/{total} valittu'
     }
   }
 };
