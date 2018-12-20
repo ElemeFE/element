2ef6a56b58895efa598f7ceb791e2286191c365e
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-12-20 13:27:06 -0200

    lint fix

--- packages/content-switch-group/index.js
+++ packages/content-switch-group/index.js
@@ -1,7 +1,7 @@
 import ContentSwitchGroup from '../content-switch/src/content-switch-group';
 
 /* istanbul ignore next */
-ContentSwitchGroup.install = function (Vue) {
+ContentSwitchGroup.install = function(Vue) {
   Vue.component(ContentSwitchGroup.name, ContentSwitchGroup);
 };
 
--- packages/content-switch/index.js
+++ packages/content-switch/index.js
@@ -1,7 +1,7 @@
 import ContentSwitch from './src/content-switch.vue';
 
 /* istanbul ignore next */
-ContentSwitch.install = function install (Vue) {
+ContentSwitch.install = function install(Vue) {
   Vue.component(ContentSwitch.name, ContentSwitch);
 };
 
