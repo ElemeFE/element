a8aa4f395b1ce75deb17038118d8dbe23ba581f3
vladyslav.loza <vladyslav.loza>
2018-12-21 12:21:10 +0200

    add suffix slot to select component

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -95,6 +95,7 @@
         <slot name="prefix"></slot>
       </template>
       <template slot="suffix">
+        <slot name="suffix"></slot>
         <i v-show="!showClose" :class="['el-select__caret', 'el-input__icon', 'el-icon-' + iconClass]"></i>
         <i v-if="showClose" class="el-select__caret el-input__icon el-icon-circle-close" @click="handleClearClick"></i>
       </template>
