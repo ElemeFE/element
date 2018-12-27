282647e6b52331d60a7bb587c26170c7315a1c9f
huangjinqiang <huangjinqiang@2980.com>
2018-12-27 16:53:34 +0800

    popper：fix that placement(in tooltip etc) not work

--- src/utils/vue-popper.js
+++ src/utils/vue-popper.js
@@ -9,7 +9,7 @@ const stop = e => e.stopPropagation();
 /**
  * @param {HTMLElement} [reference=$refs.reference] - The reference element used to position the popper.
  * @param {HTMLElement} [popper=$refs.popper] - The HTML element used as popper, or a configuration used to generate the popper.
- * @param {String} [placement=button] - Placement of the popper accepted values: top(-start, -end), right(-start, -end), bottom(-start, -end), left(-start, -end)
+ * @param {String} [placement=bottom] - Placement of the popper accepted values: top(-start, -end), right(-start, -end), bottom(-start, -end), left(-start, -end)
  * @param {Number} [offset=0] - Amount of pixels the popper will be shifted (can be negative).
  * @param {Boolean} [visible=false] Visibility of the popup element.
  * @param {Boolean} [visible-arrow=false] Visibility of the arrow, no style.
@@ -81,7 +81,7 @@ export default {
   methods: {
     createPopper() {
       if (this.$isServer) return;
-      this.currentPlacement = this.currentPlacement || this.placement;
+      this.currentPlacement = this.placement || this.currentPlacement;
       if (!/^(top|bottom|left|right)(-start|-end)?$/g.test(this.currentPlacement)) {
         return;
       }
