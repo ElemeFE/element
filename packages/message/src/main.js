38fc0c4417bf0f75e341d76f3289b5d2add2717d
jackyzm <jackyzm@foxmail.com>
2019-01-14 13:53:28 +0800

    fix: message index

--- packages/message/src/main.js
+++ packages/message/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Main from './main.vue';
+import { PopupManager } from 'element-ui/src/utils/popup';
 import { isVNode } from 'element-ui/src/utils/vdom';
 let MessageConstructor = Vue.extend(Main);
 
@@ -8,72 +9,73 @@ let instances = [];
 let seed = 1;
 
 const Message = function(options) {
-  if (Vue.prototype.$isServer) return;
-  options = options || {};
-  if (typeof options === 'string') {
-    options = { message: options };
-  }
-  let userOnClose = options.onClose;
-  let id = 'message_' + seed++;
+	if (Vue.prototype.$isServer) return;
+	options = options || {};
+	if (typeof options === 'string') {
+		options = { message: options };
+	}
+	let userOnClose = options.onClose;
+	let id = 'message_' + seed++;
 
-  options.onClose = function() {
-    Message.close(id, userOnClose);
-  };
-  instance = new MessageConstructor({
-    data: options
-  });
-  instance.id = id;
-  if (isVNode(instance.message)) {
-    instance.$slots.default = [instance.message];
-    instance.message = null;
-  }
-  instance.vm = instance.$mount();
-  instance.vm.visible = true;
-  instance.dom = instance.vm.$el;
+	options.onClose = function() {
+		Message.close(id, userOnClose);
+	};
+	instance = new MessageConstructor({
+		data: options,
+	});
+	instance.id = id;
+	if (isVNode(instance.message)) {
+		instance.$slots.default = [instance.message];
+		instance.message = null;
+	}
+	instance.vm = instance.$mount();
+	instance.vm.visible = true;
+	instance.dom = instance.vm.$el;
+	instance.dom.style.zIndex = PopupManager.nextZIndex();
 
-  const messageBox = document.getElementsByClassName('el-message-content')[0];
-  if (messageBox) {
-    messageBox.appendChild(instance.vm.$el);
-    document.body.appendChild(messageBox);
-  } else {
-    const messageBox = document.createElement('div');
-    messageBox.className = 'el-message-content';
-    messageBox.appendChild(instance.vm.$el);
-    document.body.appendChild(messageBox);
-  }
+	const messageBox = document.getElementsByClassName('el-message-content')[0];
+	if (messageBox) {
+		messageBox.appendChild(instance.vm.$el);
+		document.body.appendChild(messageBox);
+	} else {
+		const messageBox = document.createElement('div');
+		messageBox.className = 'el-message-content';
+		messageBox.appendChild(instance.vm.$el);
+		document.body.appendChild(messageBox);
+	}
 
-  instances.push(instance);
-  return instance.vm;
+	instances.push(instance);
+	return instance.vm;
 };
 
 ['success', 'warning', 'info', 'error'].forEach(type => {
-  Message[type] = options => {
-    if (typeof options === 'string') {
-      options = {
-        message: options
-      };
-    }
-    options.type = type;
-    return Message(options);
-  };
+	Message[type] = options => {
+		if (typeof options === 'string') {
+			options = {
+				message: options,
+			};
+		}
+		options.type = type;
+		return Message(options);
+	};
 });
 
 Message.close = function(id, userOnClose) {
-  for (let i = 0, len = instances.length; i < len; i++) {
-    if (id === instances[i].id) {
-      if (typeof userOnClose === 'function') {
-        userOnClose(instances[i]);
-      }
-      instances.splice(i, 1);
-      break;
-    }
-  }
+	for (let i = 0, len = instances.length; i < len; i++) {
+		if (id === instances[i].id) {
+			if (typeof userOnClose === 'function') {
+				userOnClose(instances[i]);
+			}
+			instances.splice(i, 1);
+			break;
+		}
+	}
 };
 
 Message.closeAll = function() {
-  for (let i = instances.length - 1; i >= 0; i--) {
-    instances[i].close();
-  }
+	for (let i = instances.length - 1; i >= 0; i--) {
+		instances[i].close();
+	}
 };
 
 export default Message;
