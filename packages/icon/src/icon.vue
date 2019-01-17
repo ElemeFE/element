fa5dd2e56fea16acfa1cd5c1400d0d3b56e8697b
catchOneW <kaiqiw666@gmail.com>
2019-01-17 10:29:46 +0800

    icon use es6 template string

--- packages/icon/src/icon.vue
+++ packages/icon/src/icon.vue
@@ -1,13 +1,12 @@
 <template>
-  <i :class="'el-icon-' + name"></i>
+  <i :class="`el-icon-${name}`"></i>
 </template>
 
 <script>
-  export default {
-    name: 'ElIcon',
-
-    props: {
-      name: String
-    }
-  };
+export default {
+  name: "ElIcon",
+  props: {
+    name: String
+  }
+};
 </script>
