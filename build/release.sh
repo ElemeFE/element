773123987b29dc072becc708914ef216310b0c8f
Zhi <zhi.cun@ele.me>
2019-01-26 20:19:30 +0800

    [build] 2.5.1

--- build/release.sh
+++ build/release.sh
@@ -16,16 +16,16 @@ then
   VERSION=$VERSION npm run dist
 
   # publish theme
-  echo "Releasing theme-chalk $VERSION ..."
-  cd packages/theme-chalk
-  npm version $VERSION --message "[release] $VERSION"
-  if [[ $VERSION =~ "beta" ]]
-  then
-    npm publish --tag beta
-  else
-    npm publish
-  fi
-  cd ../..
+  # echo "Releasing theme-chalk $VERSION ..."
+  # cd packages/theme-chalk
+  # npm version $VERSION --message "[release] $VERSION"
+  # if [[ $VERSION =~ "beta" ]]
+  # then
+  #   npm publish --tag beta
+  # else
+  #   npm publish
+  # fi
+  # cd ../..
 
   # commit
   git add -A
