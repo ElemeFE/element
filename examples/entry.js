67979f19e0dd618e45b0530d1ffdc5a381827e1e
ziyoung <dengzhiyang123@126.com>
2019-03-01 18:04:28 +0800

    fix highlight

--- examples/entry.js
+++ examples/entry.js
@@ -32,10 +32,10 @@ const router = new VueRouter({
 
 router.afterEach(route => {
   // https://github.com/highlightjs/highlight.js/issues/909#issuecomment-131686186
-  setTimeout(() => {
-    var blocks = document.querySelectorAll('pre code:not(.hljs)');
+  Vue.nextTick(() => {
+    const blocks = document.querySelectorAll('pre code:not(.hljs)');
     Array.prototype.forEach.call(blocks, hljs.highlightBlock);
-  }, 30);
+  });
   const data = title[route.meta.lang];
   for (let val in data) {
     if (new RegExp('^' + val, 'g').test(route.name)) {
