e89589b537847b143f794d6d07049313411f0e8f
Hao Li <haol@seagroup.com>
2019-03-14 14:09:55 +0800

    fix issue 14663 - slot in ElPagination is not updating

--- packages/pagination/src/pagination.js
+++ packages/pagination/src/pagination.js
@@ -77,7 +77,7 @@ export default {
       pager: <pager currentPage={ this.internalCurrentPage } pageCount={ this.internalPageCount } pagerCount={ this.pagerCount } on-change={ this.handleCurrentChange } disabled={ this.disabled }></pager>,
       next: <next></next>,
       sizes: <sizes pageSizes={ this.pageSizes }></sizes>,
-      slot: <my-slot></my-slot>,
+      slot: <slot>{ this.$slots.default }</slot>,
       total: <total></total>
     };
     const components = layout.split(',').map((item) => item.trim());
