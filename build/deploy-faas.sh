3fc5b5a140410fd6edc49d33aabc3b644371096c
iamkun <kunhello@outlook.com>
2019-04-03 16:03:47 +0800

    fix: Fix faas deploy mkdir error (#14952)

--- build/deploy-faas.sh
+++ build/deploy-faas.sh
@@ -7,7 +7,7 @@ git clone --depth 1 -b gh-pages --single-branch https://github.com/ElemeFE/eleme
 
 # build sub folder
 SUB_FOLDER='2.7'
-mkdir $SUB_FOLDER
+mkdir -p $SUB_FOLDER
 rm -rf *.js *.css *.map static
 rm -rf $SUB_FOLDER/**
 cp -rf ../../examples/element-ui/** .
