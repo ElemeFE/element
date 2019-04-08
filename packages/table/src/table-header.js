2b0bfe5e71ff272491bb328d3238915358fd9725
Joel Rocha <joelxr@gmail.com>
2019-04-08 13:09:02 -0300

    Table: Fix sort-change behaviour when sort condition is null (#15010)

--- packages/table/src/table-header.js
+++ packages/table/src/table-header.js
@@ -501,8 +501,6 @@ export default {
 
       if (!order) {
         sortOrder = column.order = null;
-        states.sortingColumn = null;
-        sortProp = null;
       } else {
         sortOrder = column.order = order;
       }
