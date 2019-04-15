4a51af4b3959f6a1add2b44f7cb2f20d920bca66
lisheng <lisheng_token@outlook.com>
2019-04-15 18:32:43 +0800

    loading 修复loading状态更新过快的情况下，loading的dom没有发生变化

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div style="margin: 20px;">
-    <el-input v-model="input" placeholder="请输入内容"></el-input>
+  <div>
+    <div 
+      style="width: 400px;height: 400px" 
+      v-loading="loading" 
+      element-loading-text="拼命加载中"
+      element-loading-spinner="el-icon-loading"
+      element-loading-background="rgba(0, 0, 0, 0.8)">
+    </div>
+    <el-button @click="clickMe">狂点我</el-button>
   </div>
 </template>
 
@@ -8,8 +15,18 @@
   export default {
     data() {
       return {
-        input: 'Hello Element UI!'
+        loading: true
       };
-    }
+    },
+    mounted() {
+      setTimeout(() => {
+        this.loading = false
+      }, 3000)
+    },
+    methods: {
+      clickMe() {
+        this.loading = true
+      }
+    },
   };
 </script>
