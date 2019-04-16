2c2f1c49359524b47ff6a584fd12ab977a3ffa8a
ziyoung <dengzhiyang123@126.com>
2019-04-16 17:52:21 +0800

    update docs and test case

--- test/unit/specs/calendar.spec.js
+++ test/unit/specs/calendar.spec.js
@@ -45,5 +45,26 @@ describe('Calendar', () => {
     expect(rows.length).to.equal(3);
     expect(vm.$el.querySelector('.el-calendar__button-group')).to.be.a('null');
   });
+
+  it('range tow monthes', async() => {
+    vm = createVue({
+      template: `
+      <el-calendar :range="['2019-04-15', '2019-05-19']"></el-calendar>
+      `
+    }, true);
+    const titleEl = vm.$el.querySelector('.el-calendar__title');
+    expect(/2019.*4/.test(titleEl.innerText)).to.be.true;
+    const dateTables = vm.$el.querySelectorAll('.el-calendar-table.is-range');
+    expect(dateTables.length).to.be.equal(2);
+    const rows = vm.$el.querySelectorAll('.el-calendar-table__row');
+    expect(rows.length).to.equal(5);
+    const cell = rows[rows.length - 1].firstElementChild();
+    cell.click();
+
+    await waitImmediate();
+
+    expect(/2019.*5/.test(titleEl.innerText)).to.be.true;
+    expect(cell.classList.contains('.is-selected')).to.be.true;
+  });
 });
 
