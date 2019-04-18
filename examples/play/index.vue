9ccc36e058385210d16509a4da03f847d0e6cd95
lisheng <lisheng_token@outlook.com>
2019-04-18 19:56:20 +0800

    Switch: Label with el-switch repeating event(fix #14959)

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -8,6 +8,16 @@
       element-loading-background="rgba(0, 0, 0, 0.8)">
     </div>
     <el-button @click="clickMe">狂点我</el-button>
+    <p >
+      <label @click="nativeclick">
+        <el-switch v-model="val1" @change="log" @input="log"/>
+        label wrapping switch
+      </label>
+    </p>
+    <p>
+      <el-switch id="switch" v-model="val2" @change="log" @input="log"/>
+      <label for="switch">label next to switch</label>
+    </p>
   </div>
 </template>
 
@@ -15,7 +25,9 @@
   export default {
     data() {
       return {
-        loading: true
+        loading: true,
+        val1: true,
+        val2: true,
       };
     },
     mounted() {
@@ -26,6 +38,12 @@
     methods: {
       clickMe() {
         this.loading = true
+      },
+      log($event) {
+        console.log($event)
+      },
+      nativeclick() {
+        console.log('native')
       }
     },
   };
