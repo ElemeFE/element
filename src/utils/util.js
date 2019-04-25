959b7b4698970f9c504d2eca508b6de34d680a50
Simona <36978416+SimonaliaChen@users.noreply.github.com>
2019-04-25 16:59:18 +0800

    Fix: migrating config not compatible with camel case props and events (#15260)

--- src/utils/util.js
+++ src/utils/util.js
@@ -135,3 +135,11 @@ export const autoprefixer = function(style) {
   });
   return style;
 };
+
+export const kebabCase = function(str) {
+  const hyphenateRE = /([^-])([A-Z])/g;
+  return str
+    .replace(hyphenateRE, '$1-$2')
+    .replace(hyphenateRE, '$1-$2')
+    .toLowerCase();
+};
