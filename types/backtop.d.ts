a31cd2c21eee3a4895ce74f267150c74aee55761
iamkun <kunhello@outlook.com>
2019-05-14 12:32:06 +0800

    update types

--- types/backtop.d.ts
+++ types/backtop.d.ts
@@ -2,4 +2,15 @@ import { ElementUIComponent } from './component'
 
 /** Backtop Component */
 export declare class ElBacktop extends ElementUIComponent {
+  /** Backtop target */
+  target: string
+  
+  /** Backtop visibility height */
+  visibilityHeight: string | number
+
+  /** Backtop right position */
+  right: string | number
+
+  /** Backtop bottom position */
+  bottom: string | number
 }
