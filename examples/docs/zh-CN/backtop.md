99ca4d17c465b9dfc5abe3ccae594c480ce318d9
iamkun <kunhello@outlook.com>
2019-05-14 12:05:37 +0800

    add backtop

--- examples/docs/zh-CN/backtop.md
+++ examples/docs/zh-CN/backtop.md
@@ -1,1 +1,50 @@
-## Backtop backtop
+## Backtop 回到顶部
+
+返回页面顶部的操作按钮
+
+### 基础用法
+滑动页面即可看到右下方的按钮。
+:::demo
+```html
+<template>
+  Scroll down to see the bottom-right button.
+  <el-backtop target=".page-component__scroll .el-scrollbar__wrap"></el-backtop>
+</template>
+```
+:::
+
+### 自定义显示内容
+显示区域被固定为40px * 40px的区域, 其中的内容可支持自定义。
+:::demo
+```html
+<template>
+  Scroll down to see the bottom-right button.
+  <el-backtop target=".page-component__scroll .el-scrollbar__wrap" bottom="100">
+    <div style="{
+    height: 100%;
+    width: 100%;
+    background-color: #f2f5f6;
+    box-shadow: 0 0 6px rgba(0,0,0, .12);
+    text-align: center;
+    line-height: 40px;
+    color: #1989fa;
+    }">UP</div>
+  </el-backtop>
+</template>
+```
+:::
+
+### Attributes
+
+| 参数           | 说明                           | 类型      | 可选值                               | 默认值  |
+| -------------- | ------------------------------ | --------- | ------------------------------------ | ------- |
+| target         | 触发滚动的对象          |     `String, HTMLDivElement`  | `document.documentElement` | `document.documentElement` |
+| trigger-height | 滚动高度达到此参数值才出现`BackTop`   | `String`, `Number`  | 200   | 200   |
+| right          |  控制其显示位置, 距离页面右边距     |    `String`, `Number`| 40   |40   |
+| bottom         | 控制其显示位置, 距离页面底部距离       |`String`, `Number` |  40   |40   |
+
+### Events
+
+| 事件名        | 说明                     | 回调参数   |
+| -------      | -------                | -------    |
+| click        | 点击按钮的回调函数         | (e:event)  |
--- packages/backtop/src/main.vue
+++ packages/backtop/src/main.vue
@@ -1,9 +1,100 @@
 <template>
-  <div class="el-backtop"></div>
+  <transition name="el-fade-in">
+    <div
+      v-if="visible"
+      @click.stop="handleClick"
+      :style="{
+        'right': styleRight,
+        'bottom': styleBottom
+      }"
+      class="el-backtop">
+      <slot v-if="$slots.default"></slot>
+      <el-icon name="caret-top" v-else></el-icon>
+    </div>
+  </transition>
 </template>
 
 <script>
+import throttle from 'throttle-debounce/throttle';
+
 export default {
-  name: 'ElBacktop'
+  name: 'ElBacktop',
+
+  props: {
+    triggerHeight: {
+      type: [Number],
+      default: 200
+    },
+    target: [String],
+    right: {
+      type: [Number],
+      default: 40
+    },
+    bottom: {
+      type: [Number],
+      default: 40
+    }
+  },
+
+  data() {
+    return {
+      el: null,
+      container: null,
+      visible: false
+    };
+  },
+
+  computed: {
+    styleBottom() {
+      return `${this.bottom}px`;
+    },
+    styleRight() {
+      return `${this.right}px`;
+    }
+  },
+
+  mounted() {
+    this.init();
+    this.throttledScrollHandler = throttle(300, this.onScroll);
+    this.container.addEventListener('scroll', this.throttledScrollHandler);
+  },
+
+  methods: {
+    init() {
+      this.container = document;
+      this.el = document.documentElement;
+      if (this.target) {
+        this.el = document.querySelector(this.target);
+        if (!this.el) {
+          throw new Error(`target is not existed: ${this.target}`);
+        }
+        this.container = this.el;
+      }
+    },
+    onScroll() {
+      const scrollTop = this.el.scrollTop;
+      this.visible = scrollTop >= this.triggerHeight;
+    },
+    handleClick(e) {
+      this.scrollToTop();
+      this.$emit('click', e);
+    },
+    scrollToTop() {
+      let el = this.el;
+      let step = 0;
+      let interval = setInterval(() => {
+        if (el.scrollTop <= 0) {
+          clearInterval(interval);
+          return;
+        }
+        step += 10;
+        el.scrollTop -= step;
+      }, 20);
+    }
+  },
+
+  beforeDestroy() {
+    this.container.removeEventListener('scroll', this.throttledScrollHandler);
+  }
 };
 </script>
