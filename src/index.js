49ef487e33b093236d7541d54ccb4a9f3d4a7feb
cl199793 <cl199793@alibaba-inc.com>
2019-05-27 22:03:40 +0800

    PopupManager: fix z-index value cannot be rewritten at initial render (#15074)

--- src/index.js
+++ src/index.js
@@ -169,7 +169,7 @@ const install = function(Vue, opts = {}) {
 
   Vue.prototype.$ELEMENT = {
     size: opts.size || '',
-    zIndex: opts.zIndex || 2000
+    zIndex: opts.zIndex
   };
 
   Vue.prototype.$loading = Loading.service;
