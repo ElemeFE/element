2bf8682a82a23dfd38d82514d8ff042afaddd77d
iamkun <kunhello@outlook.com>
2019-05-30 18:23:24 +0800

    Revert "Chore: Update infiniteScroll type (#15787)" (#15793)
    This reverts commit d70ae245e6594f7abdfa91b8c17919dc864cb701.

--- types/infiniteScroll.d.ts
+++ types/infiniteScroll.d.ts
@@ -1,6 +1,6 @@
 import { VNodeDirective } from 'vue'
 
-export interface ElInfiniteScroll extends VNodeDirective {
+export interface ElLoadingDirective extends VNodeDirective {
   name: 'infinite-scroll',
   value: Function
 }
