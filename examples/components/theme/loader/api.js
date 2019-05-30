1020feba0dc181f9eb8dd4c6c2a2fd6558aebe1b
iamkun <kunhello@outlook.com>
2019-05-30 16:19:56 +0800

    update api ele

--- examples/components/theme/loader/api.js
+++ examples/components/theme/loader/api.js
@@ -6,13 +6,18 @@ const { version } = Element;
 const hostList = {
   local: 'http://localhost:3008/',
   alpha: 'https://ssr.alpha.ele.me/element-theme-server/',
-  production: 'https://ssr.ele.me/element-theme-server/'
+  production: 'https://ssr.ele.me/element-theme-server/',
+  productionEle: 'https://ssr.elenet.me/element-theme-server/'
 };
 
 const host = hostList[process.env.FAAS_ENV] || hostList.production;
 
 export const getVars = () => {
   return get(`${host}getVariable?version=${version}`);
+};
+
+export const getTestEle = () => {
+  return get(`${hostList.productionEle}getVariable?version=${version}`);
 };
 
 export const updateVars = (data, cb) => {
