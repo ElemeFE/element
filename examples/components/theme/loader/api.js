3e142f996df2aac3ed52681a92d83c2b7c46f95c
iamkun <kunhello@outlook.com>
2019-05-30 16:06:33 +0800

    update api host

--- examples/components/theme/loader/api.js
+++ examples/components/theme/loader/api.js
@@ -5,8 +5,8 @@ const { version } = Element;
 
 const hostList = {
   local: 'http://localhost:3008/',
-  alpha: 'https://ssr.alpha.elenet.me/element-theme-server/',
-  production: 'https://ssr.elenet.me/element-theme-server/'
+  alpha: 'https://ssr.alpha.ele.me/element-theme-server/',
+  production: 'https://ssr.ele.me/element-theme-server/'
 };
 
 const host = hostList[process.env.FAAS_ENV] || hostList.production;
