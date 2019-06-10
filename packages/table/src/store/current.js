bc107a1d396f8e2c220cc35c46e52e9dbbf07248
ziyoung <dengzhiyang123@126.com>
2019-06-10 17:49:57 +0800

    update

--- packages/table/src/store/current.js
+++ packages/table/src/store/current.js
@@ -6,7 +6,7 @@ export default {
     return {
       states: {
         // 不可响应的，设置 currentRowKey 时，data 不一定存在，也许无法算出正确的 currentRow
-        // 把该值缓存一下，当计算出正确的 currentRow 时，把该值重置为 null
+        // 把该值缓存一下，当用户点击修改 currentRow 时，把该值重置为 null
         _currentRowKey: null,
         currentRow: null
       }
