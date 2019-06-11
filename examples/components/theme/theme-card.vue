67d4c9ee9eeec0a5610fe9eecb2ff4128e266b7d
Zhi Cun <island205@gmail.com>
2019-06-11 19:35:43 +0800

    Theme: Refine GA track & Update footer link forward to online theme roller (#16007)

--- examples/components/theme/theme-card.vue
+++ examples/components/theme/theme-card.vue
@@ -152,7 +152,7 @@
   }
   &.is-upload:hover {
     box-shadow: none;
-  } 
+  }
 
   &:hover {
     box-shadow: 0 0 10px 0 #999;
@@ -172,10 +172,10 @@
           <span>{{getActionDisplayName('upload-theme')}}</span>
         </div>
       </div>
-      <input 
-        class="el-upload__input" 
-        type="file" 
-        ref="input"  
+      <input
+        class="el-upload__input"
+        type="file"
+        ref="input"
         @change="uploadAction"
         accept="application/json"
       />
@@ -224,8 +224,8 @@
                 <el-dropdown-menu slot="dropdown">
                   <el-dropdown-item command="rename">{{getActionDisplayName('rename-theme')}}</el-dropdown-item>
                   <el-dropdown-item command="copy">{{getActionDisplayName('copy-theme')}}</el-dropdown-item>
-                  <el-dropdown-item 
-                      command="delete" 
+                  <el-dropdown-item
+                      command="delete"
                       style="color: #F56C6C;"
                     >
                       {{getActionDisplayName('delete-theme')}}
@@ -325,7 +325,7 @@ export default {
           });
           break;
         case 'download':
-          bus.$emit(ACTION_DOWNLOAD_THEME, this.theme);
+          bus.$emit(ACTION_DOWNLOAD_THEME, this.theme, this.config.name);
           break;
         default:
           this.$emit('action', e, this.config);
