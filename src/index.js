922e7823d56ab7a7caeb047286f8f779eff3230f
cl199793 <cl199793@alibaba-inc.com>
2019-06-18 18:39:38 +0800

    fix issue

--- src/index.js
+++ src/index.js
@@ -169,7 +169,7 @@ const install = function(Vue, opts = {}) {
 
   Vue.prototype.$ELEMENT = {
     size: opts.size || '',
-    zIndex: opts.zIndex
+    zIndex: opts.zIndex || 2000
   };
 
   Vue.prototype.$loading = Loading.service;
