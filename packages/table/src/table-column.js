046fa9a1ed609256d675606564496874d9205249
a631807682 <631807682@qq.com>
2019-06-28 11:10:54 +0800

    fix(packages/table/src/table-column): repeated basic prop.

--- packages/table/src/table-column.js
+++ packages/table/src/table-column.js
@@ -279,7 +279,7 @@ export default {
       index: this.index
     };
 
-    const basicProps = ['columnKey', 'label', 'className', 'labelClassName', 'type', 'renderHeader', 'resizable', 'formatter', 'fixed', 'resizable'];
+    const basicProps = ['columnKey', 'label', 'className', 'labelClassName', 'type', 'renderHeader', 'formatter', 'fixed', 'resizable'];
     const sortProps = ['sortMethod', 'sortBy', 'sortOrders'];
     const selectProps = ['selectable', 'reserveSelection'];
     const filterProps = ['filterMethod', 'filters', 'filterMultiple', 'filterOpened', 'filteredValue', 'filterPlacement'];
