68073d9612d7230a2e58b3964a6972893b870c63
a631807682 <631807682@qq.com>
2019-06-28 13:49:18 +0800

    fix(packages/table/src/table-column): fixed #16270

--- packages/table/src/table-column.js
+++ packages/table/src/table-column.js
@@ -196,7 +196,7 @@ export default {
     },
 
     registerNormalWatchers() {
-      const props = ['label', 'property', 'filters', 'filterMultiple', 'sortable', 'index', 'formatter', 'className', 'labelClassName'];
+      const props = ['label', 'property', 'filters', 'filterMultiple', 'sortable', 'index', 'formatter', 'className', 'labelClassName', 'showOverflowTooltip'];
       // 一些属性具有别名
       const aliases = {
         prop: 'property',
