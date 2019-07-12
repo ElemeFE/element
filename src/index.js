08967a15e531d4f9e12ed6514ae0f738f177a042
jingjing <2455067970@qq.com>
2019-07-12 10:45:35 +0800

    fix: 跨域问题

--- src/index.js
+++ src/index.js
@@ -188,10 +188,6 @@ const install = function(Vue, opts = {}) {
 
 };
 
-/* 解决跨域 */
-let childDomain = document.domain;
-document.domain = childDomain;
-
 /* istanbul ignore if */
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue);
