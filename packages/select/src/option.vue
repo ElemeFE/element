418ac5bf793b451c01cc70eef99ceb3d333348a3
Zhi Cun <island205@gmail.com>
2019-07-22 14:16:13 +0800

    Select: Fix memory leak issue (#16463)

--- packages/select/src/option.vue
+++ packages/select/src/option.vue
@@ -153,6 +153,10 @@
     },
 
     beforeDestroy() {
+      let index = this.select.cachedOptions.indexOf(this);
+      if (index > -1) {
+        this.select.cachedOptions.splice(index, 1);
+      }
       this.select.onOptionDestroy(this.select.options.indexOf(this));
     }
   };
