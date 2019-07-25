1fd6fef5e83be5d3984d2b33670977e546a1a1cd
iamkun <kunhello@outlook.com>
2019-07-25 15:00:04 +0800

    update build folder

--- build/deploy-faas.sh
+++ build/deploy-faas.sh
@@ -6,7 +6,7 @@ cd temp_web
 git clone --depth 1 -b gh-pages --single-branch https://github.com/ElemeFE/element.git && cd element
 
 # build sub folder
-SUB_FOLDER='2.10'
+SUB_FOLDER='2.11'
 mkdir -p $SUB_FOLDER
 rm -rf *.js *.css *.map static
 rm -rf $SUB_FOLDER/**
