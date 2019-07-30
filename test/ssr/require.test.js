13af1f20e3c9d3eff856c4a6262f1e72770aaff5
iamkun <kunhello@outlook.com>
2019-07-30 13:34:59 +0800

    ssr test

--- test/ssr/require.test.js
+++ test/ssr/require.test.js
@@ -3,6 +3,7 @@ const path = require('path');
 try {
   process.env.VUE_ENV = 'server';
   require(path.join(process.env.PWD, './lib/index'));
+  console.log('SSR require test PASS');
 } catch (e) {
   console.error('SSR require test error');
   throw Error(e);
