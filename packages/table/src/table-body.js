0b6e090fb0b98f4235b4a9ddaef54dcd7dcc58fb
a631807682 <631807682@qq.com>
2019-08-13 18:16:43 +0800

    style: update comments

--- packages/table/src/table-body.js
+++ packages/table/src/table-body.js
@@ -321,7 +321,8 @@ export default {
         rowClasses.push('el-table__row--level-' + treeRowData.level);
         display = treeRowData.display;
       }
-      // https://github.com/ElemeFE/element/issues/16995
+      // display 不是一个被观察的属性，并且指令 v-show 会覆盖 el.style.display。
+      // 使用 :style 代替 v-show https://github.com/ElemeFE/element/issues/16995
       let displayStyle = display ? null : {
         display: 'none'
       };
