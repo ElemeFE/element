9574ced0076875125ead016ca19dd69b6b1f3a01
Bryan Matthews <mattheyan@gmail.com>
2019-09-19 11:56:38 -0400

    Test: Fix test assertion now that hours are not always zero-padded

--- test/unit/specs/time-picker.spec.js
+++ test/unit/specs/time-picker.spec.js
@@ -186,9 +186,9 @@ describe('TimePicker', () => {
 
     const list = timePicker.picker.$el.querySelectorAll('.el-time-spinner__list');
 
-    // Assert that the hours list is 1-12 without AM/PM suffix
+    // Assert that the hours list is 1-12 with AM and PM suffixes
     const hours = Array.prototype.map.call(list[0].querySelectorAll('.el-time-spinner__item'), (i) => i.innerText);
-    expect(hours.join(',')).to.equal('12 AM,01 AM,02 AM,03 AM,04 AM,05 AM,06 AM,07 AM,08 AM,09 AM,10 AM,11 AM,12 PM,01 PM,02 PM,03 PM,04 PM,05 PM,06 PM,07 PM,08 PM,09 PM,10 PM,11 PM');
+    expect(hours.join(',')).to.equal('12 AM,1 AM,2 AM,3 AM,4 AM,5 AM,6 AM,7 AM,8 AM,9 AM,10 AM,11 AM,12 PM,1 PM,2 PM,3 PM,4 PM,5 PM,6 PM,7 PM,8 PM,9 PM,10 PM,11 PM');
 
     // Assert that the list is "AM" (active) and "PM" as expected
     const amPmEl = list[3];
