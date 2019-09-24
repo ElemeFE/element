a5a597f35c61d912c16aae1c778a0018dfa3ddb9
iamkun <kunhello@outlook.com>
2019-09-24 19:52:18 +0800

    init type

--- types/popconfirm.d.ts
+++ types/popconfirm.d.ts
@@ -1,5 +1,29 @@
 import { ElementUIComponent } from './component'
+import { ElPopover } from './popover'
 
 /** Popconfirm Component */
-export declare class ElPopconfirm extends ElementUIComponent {
+export declare class ElPopconfirm extends ElPopover {
+  /** Popconfirm title */
+  title: string
+
+  /** Popconfirm ok text */
+  okText: string
+  
+  /** Popconfirm cancel text */
+  cancelText: string
+
+  /** Popconfirm ok type */
+  okType: string
+
+  /** Popconfirm cancal type */
+  cancelType: string
+
+  /** Popconfirm icon */
+  icon: string
+
+  /** Popconfirm icon color */
+  iconColor: string
+
+  /** Popconfirm hide icon */
+  hideIcon: boolean
 }
