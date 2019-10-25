8773fbb51e50c3fe16a3521696ab178d643ba7d5
stepan.beznosikov <stepan.beznosikov@ltlatn.com>
2019-10-25 12:44:22 +0300

    Select: Select component trembles on zoom (#17856)

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -648,12 +648,15 @@
           let input = [].filter.call(inputChildNodes, item => item.tagName === 'INPUT')[0];
           const tags = this.$refs.tags;
           const sizeInMap = this.initialInputHeight || 40;
-          input.style.height = this.selected.length === 0
+          const newHeight = this.selected.length === 0
             ? sizeInMap + 'px'
             : Math.max(
               tags ? (tags.clientHeight + (tags.clientHeight > sizeInMap ? 6 : 0)) : 0,
               sizeInMap
             ) + 'px';
+          if (Math.abs(parseInt(input.style.height || 0, 10) - parseInt(newHeight || 0, 10)) > 1) {
+            input.style.height = newHeight
+          }
           if (this.visible && this.emptyText !== false) {
             this.broadcast('ElSelectDropdown', 'updatePopper');
           }
