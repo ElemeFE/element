80e69666eb34d3ba96b832edd107068d404e2c0a
foltyn <miroslav.foltyn01@upol.cz>
2019-11-06 09:00:22 +0100

    aria-controls not being mapped to list selection

--- packages/autocomplete/src/autocomplete.vue
+++ packages/autocomplete/src/autocomplete.vue
@@ -271,7 +271,7 @@
       let $input = this.getInput();
       $input.setAttribute('role', 'textbox');
       $input.setAttribute('aria-autocomplete', 'list');
-      $input.setAttribute('aria-controls', 'id');
+      $input.setAttribute('aria-controls', this.id);
       $input.setAttribute('aria-activedescendant', `${this.id}-item-${this.highlightedIndex}`);
     },
     beforeDestroy() {
