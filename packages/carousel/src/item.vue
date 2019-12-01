2aa383d52d55d8312645dedde27f03c487a1f2ce
IceFox <icefox@appledeMBP.lan>
2019-12-01 20:49:01 +0800

    Carousel: fix console typo bug

--- packages/carousel/src/item.vue
+++ packages/carousel/src/item.vue
@@ -88,7 +88,7 @@
         }
         if (parentType === 'card') {
           if (parentDirection === 'vertical') {
-            console.warn('[Element Warn][Carousel]vertical directionis not supported in card mode');
+            console.warn('[Element Warn][Carousel]vertical direction is not supported in card mode');
           }
           this.inStage = Math.round(Math.abs(index - activeIndex)) <= 1;
           this.active = index === activeIndex;
