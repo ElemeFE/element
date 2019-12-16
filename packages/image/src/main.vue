898f84994375096a603cf406cb8e65b2fc3b9853
Baran <me@baran.wang>
2019-12-16 14:42:32 +0800

    Image: 图片替换为 picture 组件 支持传入 source

--- packages/image/src/main.vue
+++ packages/image/src/main.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="el-image">
+  <picture class="el-image">
     <slot v-if="loading" name="placeholder">
       <div class="el-image__placeholder"></div>
     </slot>
     <slot v-else-if="error" name="error">
       <div class="el-image__error">{{ t('el.image.error') }}</div>
     </slot>
-    <img
-      v-else
-      class="el-image__inner"
-      v-bind="$attrs"
-      v-on="$listeners"
-      @click="clickHandler"
-      :src="src"
-      :style="imageStyle"
-      :class="{ 'el-image__inner--center': alignCenter, 'el-image__preview': preview }">
+    <template v-else>
+      <slot />
+      <img
+        class="el-image__inner"
+        v-bind="$attrs"
+        v-on="$listeners"
+        @click="clickHandler"
+        :src="src"
+        :style="imageStyle"
+        :class="{ 'el-image__inner--center': alignCenter, 'el-image__preview': preview }">
+    </template>
     <template v-if="preview">
       <image-viewer :z-index="zIndex" :initial-index="imageIndex" v-show="showViewer" :on-close="closeViewer" :url-list="previewSrcList"/>
     </template>
-  </div>
+  </picture>
 </template>
 
 <script>
