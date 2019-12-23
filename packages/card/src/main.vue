1114898f33e703047dc9459b207526de9911e31b
Alex <alexk.mammut@gmail.com>
2019-12-23 17:16:47 +0100

    Card: Added on click event

--- packages/card/src/main.vue
+++ packages/card/src/main.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="el-card" :class="shadow ? 'is-' + shadow + '-shadow' : 'is-always-shadow'">
-    <div class="el-card__header" v-if="$slots.header || header">
-      <slot name="header">{{ header }}</slot>
+    <div class="el-card" :class="shadow ? 'is-' + shadow + '-shadow' : 'is-always-shadow'" @click="handleClick">
+        <div class="el-card__header" v-if="$slots.header || header">
+            <slot name="header">{{ header }}</slot>
+        </div>
+        <div class="el-card__body" :style="bodyStyle">
+            <slot></slot>
+        </div>
     </div>
-    <div class="el-card__body" :style="bodyStyle">
-      <slot></slot>
-    </div>
-  </div>
 </template>
 
 <script>
-  export default {
-    name: 'ElCard',
-    props: {
-      header: {},
-      bodyStyle: {},
-      shadow: {
-        type: String
-      }
-    }
-  };
+    export default {
+        name: 'ElCard',
+        props: {
+            header: {},
+            bodyStyle: {},
+            shadow: {
+                type: String
+            }
+        },
+
+        methods: {
+            handleClick(evt) {
+                this.$emit('click', evt);
+            }
+        }
+    };
 </script>
