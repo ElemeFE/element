b15c289524b6ea312de3e7342babf5b21a5a8398
markyong <mayyyyyyyong@gmail.com>
2020-04-05 21:53:40 +0800

    Badge: fix badge content default class name

--- packages/badge/src/main.vue
+++ packages/badge/src/main.vue
@@ -7,7 +7,7 @@
         v-text="content"
         class="el-badge__content"
         :class="[
-          'el-badge__content--' + type,
+          type ? 'el-badge__content--' + type : '',
           {
             'is-fixed': $slots.default,
             'is-dot': isDot
