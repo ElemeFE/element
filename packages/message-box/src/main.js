b2a1e441e102bfd05e60d08dd437847546c3a398
jx <jx@jvxiang.com>
2020-04-22 15:36:04 +0800

    添加支持默认选中cancel的选项

--- packages/message-box/src/main.js
+++ packages/message-box/src/main.js
@@ -30,187 +30,213 @@ const defaults = {
   dangerouslyUseHTMLString: false,
   center: false,
   roundButton: false,
-  distinguishCancelAndClose: false
-};
+  distinguishCancelAndClose: false,
+  cancelDefualt: false,
+}
 
-import Vue from 'vue';
-import msgboxVue from './main.vue';
-import merge from 'element-ui/src/utils/merge';
-import { isVNode } from 'element-ui/src/utils/vdom';
+import Vue from 'vue'
+import msgboxVue from './main.vue'
+import merge from 'element-ui/src/utils/merge'
+import { isVNode } from 'element-ui/src/utils/vdom'
 
-const MessageBoxConstructor = Vue.extend(msgboxVue);
+const MessageBoxConstructor = Vue.extend(msgboxVue)
 
-let currentMsg, instance;
-let msgQueue = [];
+let currentMsg, instance
+let msgQueue = []
 
-const defaultCallback = action => {
+const defaultCallback = (action) => {
   if (currentMsg) {
-    let callback = currentMsg.callback;
+    let callback = currentMsg.callback
     if (typeof callback === 'function') {
       if (instance.showInput) {
-        callback(instance.inputValue, action);
+        callback(instance.inputValue, action)
       } else {
-        callback(action);
+        callback(action)
       }
     }
     if (currentMsg.resolve) {
       if (action === 'confirm') {
         if (instance.showInput) {
-          currentMsg.resolve({ value: instance.inputValue, action });
+          currentMsg.resolve({ value: instance.inputValue, action })
         } else {
-          currentMsg.resolve(action);
+          currentMsg.resolve(action)
         }
-      } else if (currentMsg.reject && (action === 'cancel' || action === 'close')) {
-        currentMsg.reject(action);
+      } else if (
+        currentMsg.reject &&
+        (action === 'cancel' || action === 'close')
+      ) {
+        currentMsg.reject(action)
       }
     }
   }
-};
+}
 
 const initInstance = () => {
   instance = new MessageBoxConstructor({
-    el: document.createElement('div')
-  });
+    el: document.createElement('div'),
+  })
 
-  instance.callback = defaultCallback;
-};
+  instance.callback = defaultCallback
+}
 
 const showNextMsg = () => {
   if (!instance) {
-    initInstance();
+    initInstance()
   }
-  instance.action = '';
+  instance.action = ''
 
   if (!instance.visible || instance.closeTimer) {
     if (msgQueue.length > 0) {
-      currentMsg = msgQueue.shift();
+      currentMsg = msgQueue.shift()
 
-      let options = currentMsg.options;
+      let options = currentMsg.options
       for (let prop in options) {
         if (options.hasOwnProperty(prop)) {
-          instance[prop] = options[prop];
+          instance[prop] = options[prop]
         }
       }
       if (options.callback === undefined) {
-        instance.callback = defaultCallback;
+        instance.callback = defaultCallback
       }
 
-      let oldCb = instance.callback;
+      let oldCb = instance.callback
       instance.callback = (action, instance) => {
-        oldCb(action, instance);
-        showNextMsg();
-      };
-      if (isVNode(instance.message)) {
-        instance.$slots.default = [instance.message];
-        instance.message = null;
-      } else {
-        delete instance.$slots.default;
+        oldCb(action, instance)
+        showNextMsg()
       }
-      ['modal', 'showClose', 'closeOnClickModal', 'closeOnPressEscape', 'closeOnHashChange'].forEach(prop => {
+      if (isVNode(instance.message)) {
+        instance.$slots.default = [instance.message]
+        instance.message = null
+      } else {
+        delete instance.$slots.default
+      }
+      ;[
+        'modal',
+        'showClose',
+        'closeOnClickModal',
+        'closeOnPressEscape',
+        'closeOnHashChange',
+      ].forEach((prop) => {
         if (instance[prop] === undefined) {
-          instance[prop] = true;
+          instance[prop] = true
         }
-      });
-      document.body.appendChild(instance.$el);
+      })
+      document.body.appendChild(instance.$el)
 
       Vue.nextTick(() => {
-        instance.visible = true;
-      });
+        instance.visible = true
+      })
     }
   }
-};
+}
 
-const MessageBox = function(options, callback) {
-  if (Vue.prototype.$isServer) return;
+const MessageBox = function (options, callback) {
+  if (Vue.prototype.$isServer) return
   if (typeof options === 'string' || isVNode(options)) {
     options = {
-      message: options
-    };
+      message: options,
+    }
     if (typeof arguments[1] === 'string') {
-      options.title = arguments[1];
+      options.title = arguments[1]
     }
   } else if (options.callback && !callback) {
-    callback = options.callback;
+    callback = options.callback
   }
 
   if (typeof Promise !== 'undefined') {
-    return new Promise((resolve, reject) => { // eslint-disable-line
+    return new Promise((resolve, reject) => {
+      // eslint-disable-line
       msgQueue.push({
         options: merge({}, defaults, MessageBox.defaults, options),
         callback: callback,
         resolve: resolve,
-        reject: reject
-      });
+        reject: reject,
+      })
 
-      showNextMsg();
-    });
+      showNextMsg()
+    })
   } else {
     msgQueue.push({
       options: merge({}, defaults, MessageBox.defaults, options),
-      callback: callback
-    });
+      callback: callback,
+    })
 
-    showNextMsg();
+    showNextMsg()
   }
-};
+}
 
-MessageBox.setDefaults = defaults => {
-  MessageBox.defaults = defaults;
-};
+MessageBox.setDefaults = (defaults) => {
+  MessageBox.defaults = defaults
+}
 
 MessageBox.alert = (message, title, options) => {
   if (typeof title === 'object') {
-    options = title;
-    title = '';
+    options = title
+    title = ''
   } else if (title === undefined) {
-    title = '';
+    title = ''
   }
-  return MessageBox(merge({
-    title: title,
-    message: message,
-    $type: 'alert',
-    closeOnPressEscape: false,
-    closeOnClickModal: false
-  }, options));
-};
+  return MessageBox(
+    merge(
+      {
+        title: title,
+        message: message,
+        $type: 'alert',
+        closeOnPressEscape: false,
+        closeOnClickModal: false,
+      },
+      options
+    )
+  )
+}
 
 MessageBox.confirm = (message, title, options) => {
   if (typeof title === 'object') {
-    options = title;
-    title = '';
+    options = title
+    title = ''
   } else if (title === undefined) {
-    title = '';
+    title = ''
   }
-  return MessageBox(merge({
-    title: title,
-    message: message,
-    $type: 'confirm',
-    showCancelButton: true
-  }, options));
-};
+  return MessageBox(
+    merge(
+      {
+        title: title,
+        message: message,
+        $type: 'confirm',
+        showCancelButton: true,
+      },
+      options
+    )
+  )
+}
 
 MessageBox.prompt = (message, title, options) => {
   if (typeof title === 'object') {
-    options = title;
-    title = '';
+    options = title
+    title = ''
   } else if (title === undefined) {
-    title = '';
+    title = ''
   }
-  return MessageBox(merge({
-    title: title,
-    message: message,
-    showCancelButton: true,
-    showInput: true,
-    $type: 'prompt'
-  }, options));
-};
+  return MessageBox(
+    merge(
+      {
+        title: title,
+        message: message,
+        showCancelButton: true,
+        showInput: true,
+        $type: 'prompt',
+      },
+      options
+    )
+  )
+}
 
 MessageBox.close = () => {
-  instance.doClose();
-  instance.visible = false;
-  msgQueue = [];
-  currentMsg = null;
-};
+  instance.doClose()
+  instance.visible = false
+  msgQueue = []
+  currentMsg = null
+}
 
-export default MessageBox;
-export { MessageBox };
+export default MessageBox
+export { MessageBox }
