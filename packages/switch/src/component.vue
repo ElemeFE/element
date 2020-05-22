d3f62b36f32ef23bb0bf4c17904023fd55482ccb
Edwin Betancourt <EdwinBetanc0urt@hotmail.com>
2020-05-22 16:08:58 -0400

    delete duplicated logic.

--- packages/switch/src/component.vue
+++ packages/switch/src/component.vue
@@ -117,9 +117,7 @@
     },
     watch: {
       checked() {
-        if (this.$refs.input) {
-          this.$refs.input.checked = this.checked;
-        }
+        this.$refs.input.checked = this.checked;
         if (this.activeColor || this.inactiveColor) {
           this.setBackgroundColor();
         }
@@ -136,7 +134,6 @@
         this.$nextTick(() => {
           // set input's checked property
           // in case parent refuses to change component's value
-          this.$refs.input.checked = this.checked;
           if (this.$refs.input) {
             this.$refs.input.checked = this.checked;
           }
