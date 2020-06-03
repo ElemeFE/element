aa2e83de68187d8e89aeea5558ff394ca0494ff7
zhuhq <616928019@qq.com>
2020-06-03 21:11:40 +0800

    :lipstick: Adds a theme color parameter to the icon component

--- packages/icon/src/icon.vue
+++ packages/icon/src/icon.vue
@@ -1,13 +1,26 @@
 <template>
-  <i :class="'el-icon-' + name"></i>
+  <i :class="getColor() + 'el-icon-' + name"></i>
 </template>
 
 <script>
-  export default {
-    name: 'ElIcon',
+export default {
+  name: "ElIcon",
 
-    props: {
-      name: String
+  props: {
+    name: String,
+    type: {
+      type: String,
+      default: "default"
     }
-  };
+  },
+  computed: {
+    getColor(){
+      const {type} = this;
+      if(type.length){
+        return `el-link el-link--${type} `
+      }
+      return ''
+    }
+  },
+};
 </script>
