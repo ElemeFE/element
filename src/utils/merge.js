101ba754c9e5378e679b1f83d08b2ea5ef30fac7
jason <1479235292@qq.com>
2020-06-21 10:14:31 +0800

    src/utils/merge.js: fix a bug of function hasOwnProperty() directly called on an object

--- src/utils/merge.js
+++ src/utils/merge.js
@@ -2,7 +2,7 @@ export default function(target) {
   for (let i = 1, j = arguments.length; i < j; i++) {
     let source = arguments[i] || {};
     for (let prop in source) {
-      if (source.hasOwnProperty(prop)) {
+      if (Object.prototype.hasOwnProperty.call(source, prop)) {
         let value = source[prop];
         if (value !== undefined) {
           target[prop] = value;
