57a46ee344699047fb6e6588d7980ea5f741e1ec
Rajendra Kumar R Yadav <rajendra.ecti@gmail.com>
2020-06-24 13:02:48 +0530

    locale: Added Hindi Locale

---
+++ src/locale/lang/hi.js
@@ -0,0 +1,123 @@
+'use strict';
+
+exports.__esModule = true;
+exports.default = {
+  el: {
+    colorpicker: {
+      confirm: 'ठीक',
+      clear: 'हटाना'
+    },
+    datepicker: {
+      now: 'अभी',
+      today: 'आज',
+      cancel: 'रद्द करना',
+      clear: 'हटाना',
+      confirm: 'ठीक',
+      selectDate: 'तारीख़ चुनें',
+      selectTime: 'समय चुनें',
+      startDate: 'आरंभ करने की तारीख़',
+      startTime: 'शुरू करने की समय',
+      endDate: 'अंतिम तिथि',
+      endTime: 'अंतिम समय',
+      prevYear: 'पिछला वर्ष',
+      nextYear: 'अगले वर्ष',
+      prevMonth: 'पिछ्ला महिना',
+      nextMonth: 'अगले महीना',
+      year: '',
+      month1: 'जनवरी',
+      month2: 'फरवरी',
+      month3: 'मार्च',
+      month4: 'अप्रैल',
+      month5: 'मई',
+      month6: 'जून',
+      month7: 'जुलाई',
+      month8: 'अगस्त',
+      month9: 'सितंबर',
+      month10: 'अक्टूबर',
+      month11: 'नवंबर',
+      month12: 'दिसंबर',
+      week: 'week',
+      weeks: {
+        sun: 'रवि',
+        mon: 'सोम',
+        tue: 'मंगल',
+        wed: 'बुध',
+        thu: 'गुरु',
+        fri: 'शुक्र',
+        sat: 'शनि'
+      },
+      months: {
+        jan: 'जनवरी',
+        feb: 'फ़रवरी',
+        mar: 'मार्च',
+        apr: 'अप्रैल',
+        may: 'मई',
+        jun: 'जून',
+        jul: 'जुलाई',
+        aug: 'अगस्त',
+        sep: 'सितम्बर',
+        oct: 'अक्टूबर',
+        nov: 'नवम्बर',
+        dec: 'दिसम्बर'
+      }
+    },
+    select: {
+      loading: 'लोड हो रहा है',
+      noMatch: 'कोई डेटा नहीं',
+      noData: 'कोई आकड़ा नहीं है',
+      placeholder: 'चयन'
+    },
+    cascader: {
+      noMatch: 'कोई डेटा नहीं',
+      loading: 'लोड हो रहा है',
+      placeholder: 'चयन',
+      noData: 'कोई आकड़ा नहीं है'
+    },
+    pagination: {
+      goto: 'Go to',
+      pagesize: '/page',
+      total: 'टोटल {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'संदेश',
+      confirm: 'ठीक',
+      cancel: 'रद्द',
+      error: 'अवैध इनपुट'
+    },
+    upload: {
+      deleteTip: 'हटाने के लिए डिलीट दबाएं',
+      delete: 'हटाएं',
+      preview: 'पूर्वावलोकन',
+      continue: 'जारी रखें'
+    },
+    table: {
+      emptyText: 'कोई आकड़ा नहीं है',
+      confirmFilter: 'पुष्टि करें',
+      resetFilter: 'रीसेट',
+      clearFilter: 'संपूर्ण',
+      sumText: 'योग'
+    },
+    tree: {
+      emptyText: 'कोई आकड़ा नहीं है'
+    },
+    transfer: {
+      noMatch: 'कोई डेटा नहीं',
+      noData: 'कोई आकड़ा नहीं है',
+      titles: ['List 1', 'List 2'], // to be translated
+      filterPlaceholder: 'Enter keyword', // to be translated
+      noCheckedFormat: '{total} items', // to be translated
+      hasCheckedFormat: '{checked}/{total} checked' // to be translated
+    },
+    image: {
+      error: 'असफल'
+    },
+    pageHeader: {
+      title: 'Back' // to be translated
+    },
+    popconfirm: {
+      confirmButtonText: 'हाँ',
+      cancelButtonText: 'नहीं'
+    }
+  }
+};
