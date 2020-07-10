e0c70e6b9674595747bc350a47541ae8b20b650a
李通洲 <carter.li@eoitek.com>
2020-07-10 13:28:20 +0800

    v2.14.1

--- src/index.js
+++ src/index.js
@@ -198,7 +198,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  version: '2.14.0-beta12',
+  version: '2.14.1',
   locale: locale.use,
   i18n: locale.i18n,
   install,
