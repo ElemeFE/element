08d6e05b247614ff7636a2bf849997bf87f8c6e2
陈大伟 <576120983@qq.com>
2020-07-27 16:23:16 +0800

    fix(utils-clickoutside.js): 去除不必要的判断逻辑

--- src/utils/clickoutside.js
+++ src/utils/clickoutside.js
@@ -21,7 +21,6 @@ function createDocumentHandler(el, binding, vnode) {
       !mousedown.target ||
       el.contains(mouseup.target) ||
       el.contains(mousedown.target) ||
-      el === mouseup.target ||
       (vnode.context.popperElm &&
       (vnode.context.popperElm.contains(mouseup.target) ||
       vnode.context.popperElm.contains(mousedown.target)))) return;
