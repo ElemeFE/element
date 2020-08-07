f416f1676969d4a18de9a2138830e4b745581b34
bulatie <bulatie@gmail.com>
2020-08-07 11:22:40 +0800

    feat: add theme bello

---
+++ packages/theme-bello/README.md
@@ -0,0 +1,38 @@
+# element-theme-bello
+
+> element component bello theme.
+
+## Installation
+
+```shell
+npm i element-theme-bello -S
+```
+
+## Usage
+
+Use Sass import
+
+```css
+@import 'element-theme-bello';
+```
+
+Or Use webpack
+
+```javascript
+import 'element-theme-bello';
+```
+
+Or
+
+```html
+<link rel="stylesheet" href="path/to/node_modules/element-theme-bello/lib/index.css">
+```
+
+## Import on demand
+
+```javascript
+import 'element-theme-bello/lib/input.css';
+import 'element-theme-bello/lib/select.css';
+
+// ...
+```
---
+++ packages/theme-bello/gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { series, src, dest } = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssmin = require('gulp-cssmin');
+
+function compile() {
+  return src('./src/*.scss')
+    .pipe(sass.sync())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(dest('./lib'));
+}
+
+function copyfont() {
+  return src('./src/fonts/**')
+    .pipe(cssmin())
+    .pipe(dest('./lib/fonts'));
+}
+
+exports.build = series(compile, copyfont);
