291708d8ce57aea8b9b4d5b5b607a78dc6daa4cc
Andrey Dyachenko <andrey.dyachenko@zfx.com>
2020-09-17 11:53:35 +0300

    [lang] Malay language support
    Added new localization file my.js for Malay language support

---
+++ src/locale/lang/my.js
@@ -0,0 +1,120 @@
+export default {
+  el: {
+    colorpicker: {
+      confirm: 'OKEY',
+      clear: 'Jelas'
+    },
+    datepicker: {
+      now: 'Sekarang',
+      today: 'Hari ini',
+      cancel: 'Batal',
+      clear: 'Jelas',
+      confirm: 'OKEY',
+      selectDate: 'Pilih tarikh',
+      selectTime: 'Pilih masa',
+      startDate: 'Tarikh mula',
+      startTime: 'Masa mula',
+      endDate: 'Tarikh tamat',
+      endTime: 'Masa tamat',
+      prevYear: 'Tahun sebelumnya',
+      nextYear: 'Tahun hadapan',
+      prevMonth: 'Bulan lepas',
+      nextMonth: 'Bulan depan',
+      year: '',
+      month1: 'Januari',
+      month2: 'Februari',
+      month3: 'Mac',
+      month4: 'April',
+      month5: 'Mungkin',
+      month6: 'Jun',
+      month7: 'Julai',
+      month8: 'Ogos',
+      month9: 'September',
+      month10: 'Oktober',
+      month11: 'Tidakvember',
+      month12: 'Disember',
+      week: 'minggu',
+      weeks: {
+        sun: 'Min',
+        mon: 'Isn',
+        tue: 'Sel',
+        wed: 'Rab',
+        thu: 'Kha',
+        fri: 'Jum',
+        sat: 'Sab'
+      },
+      months: {
+        jan: 'Jan',
+        feb: 'Feb',
+        mar: 'Mac',
+        apr: 'Apr',
+        may: 'Mei',
+        jun: 'Jun',
+        jul: 'Jul',
+        aug: 'Ogs',
+        sep: 'Sep',
+        oct: 'Okt',
+        nov: 'Nov',
+        dec: 'Dis'
+      }
+    },
+    select: {
+      loading: 'Memuatkan',
+      noMatch: 'Tiada data yang sepadan',
+      noData: 'Tiada data',
+      placeholder: 'Pilih'
+    },
+    cascader: {
+      noMatch: 'Tiada data yang sepadan',
+      loading: 'Memuatkan',
+      placeholder: 'Pilih',
+      noData: 'Tiada data'
+    },
+    pagination: {
+      goto: 'Pergi ke',
+      pagesize: '/halaman',
+      total: 'Jumlah {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'Mesej',
+      confirm: 'OKEY',
+      cancel: 'Batal',
+      error: 'Input tidak sah'
+    },
+    upload: {
+      deleteTip: 'tekan delete untuk membuang',
+      delete: 'Padam',
+      preview: 'Pratonton',
+      continue: 'Teruskan'
+    },
+    table: {
+      emptyText: 'Tiada data',
+      confirmFilter: 'Sahkan',
+      resetFilter: 'Tetapkan semula',
+      clearFilter: 'Semua',
+      sumText: 'Jumlah'
+    },
+    tree: {
+      emptyText: 'Tiada data'
+    },
+    transfer: {
+      noMatch: 'Tiada data yang sepadan',
+      noData: 'Tiada data',
+      titles: ['Senaraikan 1', 'Senaraikan 2'], // to be translated
+      filterPlaceholder: 'Masukkan kata kunci', // to be translated
+      noCheckedFormat: '{total} barang', // to be translated
+      hasCheckedFormat: '{checked}/{total} diperiksa' // to be translated
+    },
+    image: {
+      error: 'GAGAL'
+    },
+    pageHeader: {
+      title: 'Belakang' // to be translated
+    },
+    popconfirm: {
+      confirmButtonText: 'Ya',
+      cancelButtonText: 'Tidak'
+    }
+  }
+};
