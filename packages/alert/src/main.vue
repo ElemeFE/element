1269bab0510cadcbe23faedebc8d92db66265ef1
Me===Five <46315787+horseson2018@users.noreply.github.com>
2020-09-18 10:43:49 +0800

    Fix el-alert can't open again
    Fix el-alert can't open again.

--- packages/alert/src/main.vue
+++ packages/alert/src/main.vue
@@ -3,7 +3,7 @@
     <div
       class="el-alert"
       :class="[typeClass, center ? 'is-center' : '', 'is-' + effect]"
-      v-show="visible"
+      v-show="selfVisible"
       role="alert"
     >
       <i class="el-alert__icon" :class="[ iconClass, isBigIcon ]" v-if="showIcon"></i>
@@ -62,13 +62,12 @@
 
     data() {
       return {
-        visible: true
       };
     },
 
     methods: {
       close() {
-        this.visible = false;
+        this.selfVisible = false;
         this.$emit('close');
       }
     },
@@ -88,6 +87,15 @@
 
       isBoldTitle() {
         return this.description || this.$slots.default ? 'is-bold' : '';
+      },
+
+      selfVisible: {
+        get() {
+          return this.visible
+        },
+        set(val) {
+          return val
+        }
       }
     }
   };
