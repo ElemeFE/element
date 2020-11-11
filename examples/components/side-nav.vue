02847c134c6ba8f073998f80432a1560d5b8f003
iamkun <kunhello@outlook.com>
2020-11-11 16:24:07 +0800

    chore: update

--- examples/components/side-nav.vue
+++ examples/components/side-nav.vue
@@ -122,21 +122,6 @@
     :class="{ 'is-fade': isFade }"
     :style="navStyle">
     <ul>
-      <li class="nav-item sponsors">
-        <a>{{ lang === 'zh-CN' ? '赞助商' : 'Sponsors' }}</a>
-        <ul class="pure-menu-list sub-nav">
-          <li class="nav-item" v-show="lang !== 'zh-CN'">
-            <a href="https://tipe.io/?ref=element" target="_blank">
-              <img src="~examples/assets/images/tipe.svg" alt="tipe.io">
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="sponsor" href="https://www.duohui.cn/?utm_source=element&utm_medium=web&utm_campaign=element-index" target="_blank">
-              <img src="~examples/assets/images/duohui.svg" alt="duohui">
-            </a>
-          </li>
-        </ul>
-      </li>
       <li
         class="nav-item"
         v-for="(item, key) in data"
