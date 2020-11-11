5ce89b8e00642f58f0e1942c20782d9b17276673
IlhamTahir <yili@howzhi.com>
2020-11-11 16:49:13 +0800

    i18n: Update Uighur locale (#20177)

--- src/locale/lang/ug-CN.js
+++ src/locale/lang/ug-CN.js
@@ -107,14 +107,14 @@ export default {
       hasCheckedFormat: 'تاللانغىنى {checked}/{total} تۈر'
     },
     image: {
-      error: 'FAILED' // to be translated
+      error: 'مەغلۇب بولدى'
     },
     pageHeader: {
-      title: 'Back' // to be translated
+      title: 'قايتىش'
     },
     popconfirm: {
-      confirmButtonText: 'Yes', // to be translated
-      cancelButtonText: 'No' // to be translated
+      confirmButtonText: 'ھەئە',
+      cancelButtonText: 'ياق'
     }
   }
 };
