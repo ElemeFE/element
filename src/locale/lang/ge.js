123197fa8a359786dfb46d5f5f2c8d41f3dd1a98
Piriali <48906783+Piriali@users.noreply.github.com>
2020-12-31 12:48:02 +0400

    Create ge.js

---
+++ src/locale/lang/ge.js
@@ -0,0 +1,121 @@
+  
+export default {
+  el: {
+    colorpicker: {
+      confirm: 'დიახ',
+      clear: 'დასუფთავება'
+    },
+    datepicker: {
+      now: 'ახლა',
+      today: 'დღეს',
+      cancel: 'გაუქმება',
+      clear: 'გასუფთავება',
+      confirm: 'დიახ',
+      selectDate: 'აირჩიეთ თარიღი',
+      selectTime: 'აირჩიეთ დრო',
+      startDate: 'დაწყების თარიღი',
+      startTime: 'დაწყების დრო',
+      endDate: 'დასრულების თარიღი',
+      endTime: 'დასრულების დრო',
+      prevYear: 'წინა წელი',
+      nextYear: 'მომდევნო წელი',
+      prevMonth: 'წინა თვე',
+      nextMonth: 'მომდევნო თვე',
+      year: '',
+      month1: 'იანვარი',
+      month2: 'თებერვალი',
+      month3: 'მარტი',
+      month4: 'აპრილი',
+      month5: 'მაისი',
+      month6: 'ივნისი',
+      month7: 'ივლისი',
+      month8: 'აგვისტო',
+      month9: 'სექტემბერი',
+      month10: 'ოქტომბერი',
+      month11: 'ნოემბერი',
+      month12: 'დეკემბერი',
+      week: 'კვირა',
+      weeks: {
+        sun: 'კვი',
+        mon: 'ორშ',
+        tue: 'სამ',
+        wed: 'ოთხ',
+        thu: 'ხუთ',
+        fri: 'პარ',
+        sat: 'შაბ'
+      },
+      months: {
+        jan: 'იან',
+        feb: 'თებ',
+        mar: 'მარ',
+        apr: 'აპრ',
+        may: 'მაი',
+        jun: 'ივნ',
+        jul: 'ივლ',
+        aug: 'აგვ',
+        sep: 'სექ',
+        oct: 'ოქტ',
+        nov: 'ნოე',
+        dec: 'დეკ'
+      }
+    },
+    select: {
+      loading: 'იტვირთება',
+      noMatch: 'თარიღი არაა არჩეული',
+      noData: 'მონაცემები არ არის',
+      placeholder: 'აირჩიე'
+    },
+    cascader: {
+      noMatch: 'მონაცემები არ არის',
+      loading: 'იტვირთება',
+      placeholder: 'აირჩიე',
+      noData: 'მონაცემები არ არის'
+    },
+    pagination: {
+      goto: 'გადასვლა',
+      pagesize: '/page',
+      total: 'Total {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'მესიჯი',
+      confirm: 'დიახ',
+      cancel: 'გაუქმება',
+      error: 'შეცდომა'
+    },
+    upload: {
+      deleteTip: 'აირჩიეთ გაუქმება წასაშლელად',
+      delete: 'წაშლა',
+      preview: 'წინა',
+      continue: 'გაგრძელება'
+    },
+    table: {
+      emptyText: 'მონაცემები არ არის',
+      confirmFilter: 'დასტური',
+      resetFilter: 'გადატვირთვა',
+      clearFilter: 'ყველა',
+      sumText: 'ჯამი'
+    },
+    tree: {
+      emptyText: 'მონაცემები არ არის'
+    },
+    transfer: {
+      noMatch: 'მონაცემები არ არის',
+      noData: 'მონაცემები არ არის',
+      titles: ['List 1', 'List 2'], // to be translated
+      filterPlaceholder: 'Enter keyword', // to be translated
+      noCheckedFormat: '{total} items', // to be translated
+      hasCheckedFormat: '{checked}/{total} checked' // to be translated
+    },
+    image: {
+      error: 'შეცდომა'
+    },
+    pageHeader: {
+      title: 'უკან' // to be translated
+    },
+    popconfirm: {
+      confirmButtonText: 'დიახ',
+      cancelButtonText: 'არა'
+    }
+  }
+};
