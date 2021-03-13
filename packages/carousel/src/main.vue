e36c52dcfefc47655a832d1cbe738852a9bef1bd
nekojita <bearlove@ooxx.me>
2021-03-13 21:23:35 +0800

    Carousel: reset the timer when setActiveItem method is called

--- packages/carousel/src/main.vue
+++ packages/carousel/src/main.vue
@@ -231,6 +231,11 @@ export default {
       this.timer = setInterval(this.playSlides, this.interval);
     },
 
+    resetTimer() {
+      this.pauseTimer();
+      this.startTimer();
+    },
+
     setActiveItem(index) {
       if (typeof index === 'string') {
         const filteredItems = this.items.filter(item => item.name === index);
@@ -255,6 +260,7 @@ export default {
       if (oldIndex === this.activeIndex) {
         this.resetItemPosition(oldIndex);
       }
+      this.resetTimer();
     },
 
     prev() {
