0588c8a7fec3b1884779c0e5a90342363ecf734e
mPyth <oostoya2@yahoo.com>
2021-04-06 12:17:47 +0200

    Update sr.js

--- src/locale/lang/sr.js
+++ src/locale/lang/sr.js
@@ -101,20 +101,20 @@ export default {
     transfer: {
       noMatch: 'Нема резултата',
       noData: 'Нема података',
-      titles: ['Листа 1', 'Листа 2'], // to be translated
-      filterPlaceholder: 'Унеси кључну реч', // to be translated
-      noCheckedFormat: '{total} ставки', // to be translated
-      hasCheckedFormat: '{checked}/{total} обележених' // to be translated
+      titles: ['Листа 1', 'Листа 2'],
+      filterPlaceholder: 'Унеси кључну реч',
+      noCheckedFormat: '{total} ставки',
+      hasCheckedFormat: '{checked}/{total} обележених'
     },
     image: {
-      error: 'FAILED' // to be translated
+      error: 'НИЈЕ УСПЕО'
     },
     pageHeader: {
-      title: 'Back' // to be translated
+      title: 'Назад'
     },
     popconfirm: {
-      confirmButtonText: 'Yes', // to be translated
-      cancelButtonText: 'No' // to be translated
+      confirmButtonText: 'Да',
+      cancelButtonText: 'Не'
     }
   }
 };
