da4911d5b4f0204f42308d0a80e3fac942159630
god <814179488@qq.com>
2021-04-21 10:40:06 +0800

    el-date-picker:增加精确到小时格式，yyyy-MM-dd HH

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,14 +1,29 @@
 <template>
   <div style="margin: 20px;">
     <el-input v-model="input" placeholder="请输入内容"></el-input>
+
+
+      <div class="block">
+          <span class="demonstration">默认</span>
+          <el-date-picker
+                  v-model="value1"
+                  type="datetimerange"
+                  format="yyyy-MM-dd HH"
+                  placeholder="选择日期时间">
+          </el-date-picker>
+      </div>
+
   </div>
+
+
 </template>
 
 <script>
   export default {
     data() {
       return {
-        input: 'Hello Element UI!'
+        input: 'Hello Element UI!',
+          value1:new Date()
       };
     }
   };
