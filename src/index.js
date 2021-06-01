b50de7b729b7d6e3a12a74cd83ee2538fa08cb59
cs1707 <lost1q84@gmail.com>
2021-06-01 17:06:53 +0800

    Skeleton: add skeleton component

--- src/index.js
+++ src/index.js
@@ -82,6 +82,8 @@ import CascaderPanel from '../packages/cascader-panel/index.js';
 import Avatar from '../packages/avatar/index.js';
 import Drawer from '../packages/drawer/index.js';
 import Popconfirm from '../packages/popconfirm/index.js';
+import Skeleton from '../packages/skeleton/index.js';
+import SkeletonItem from '../packages/skeleton-item/index.js';
 import locale from 'element-ui/src/locale';
 import CollapseTransition from 'element-ui/src/transitions/collapse-transition';
 
@@ -163,6 +165,8 @@ const components = [
   Avatar,
   Drawer,
   Popconfirm,
+  Skeleton,
+  SkeletonItem,
   CollapseTransition
 ];
 
@@ -284,5 +288,7 @@ export default {
   CascaderPanel,
   Avatar,
   Drawer,
-  Popconfirm
+  Popconfirm,
+  Skeleton,
+  SkeletonItem
 };
