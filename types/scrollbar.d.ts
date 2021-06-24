29fe5d784511e4e30dd406cb2472c80646143c5f
YuShifan <894402575bt@gmail.com>
2021-06-24 18:21:05 +0800

    feat(types): add scrollbar type file

---
+++ types/scrollbar.d.ts
@@ -0,0 +1,25 @@
+import { ElementUIComponent } from './component'
+
+/** Header Component */
+export declare class ElScrollbar extends ElementUIComponent {
+  /** Native type */
+  native: boolean
+
+  /** WrapStyle type */
+  wrapStyle: any
+
+  /** WrapClass type */
+  wrapClass: any
+
+  /** ViewClass type */
+  viewClass: any
+
+  /** ViewStyle type */
+  viewStyle: any
+
+  /** Noresize type */
+  noresize: boolean
+
+  /** Tag type */
+  tag: string
+}
