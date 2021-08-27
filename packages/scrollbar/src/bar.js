e7081dc7e1a17a5fcc379c0daed6044bb6647677
Tyler Trotter <tylertrotter@gmail.com>
2021-08-27 16:06:53 -0400

    Only output a scrollbar div if there's a scrollbar. (#61)
    * Only output a scrollbar div if there's a scrollbar.
    
    * Per React JSX docs

--- packages/scrollbar/src/bar.js
+++ packages/scrollbar/src/bar.js
@@ -24,6 +24,10 @@ export default {
   render(h) {
     const { size, move, bar } = this;
 
+    if (!size) {
+      return null;
+    }
+
     return (
       <div
         class={ ['el-scrollbar__bar', 'is-' + bar.key] }
