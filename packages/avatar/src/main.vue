5cebd18fce35e837764cb315612359f23dda7c98
Fengyuan Chen <chenfengyuan@outlook.com>
2021-10-08 20:06:25 +0800

    Avatar: fix browser compatibility for IE
    As IE doesn't support `Array.prototype.includes`, it will throw error: `Object doesn't support property or method 'includes'`

--- packages/avatar/src/main.vue
+++ packages/avatar/src/main.vue
@@ -7,7 +7,7 @@ export default {
       type: [Number, String],
       validator(val) {
         if (typeof val === 'string') {
-          return ['large', 'medium', 'small'].includes(val);
+          return ['large', 'medium', 'small'].indexOf(val) >= 0;
         }
         return typeof val === 'number';
       }
@@ -16,7 +16,7 @@ export default {
       type: String,
       default: 'circle',
       validator(val) {
-        return ['circle', 'square'].includes(val);
+        return ['circle', 'square'].indexOf(val) >= 0;
       }
     },
     icon: String,
