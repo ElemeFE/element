d0ed7f94d6e9d5cfff814e4d281d67cdfe9177af
好多大米 <lost1q84@gmail.com>
2021-11-17 11:09:40 +0800

    Table: fix toggleAllSelection bug when table is empty (#21456)

--- packages/table/src/config.js
+++ packages/table/src/config.js
@@ -32,7 +32,7 @@ export const cellForced = {
       return <el-checkbox
         disabled={ store.states.data && store.states.data.length === 0 }
         indeterminate={ store.states.selection.length > 0 && !this.isAllSelected }
-        nativeOn-click={ this.toggleAllSelection }
+        on-input={ this.toggleAllSelection }
         value={ this.isAllSelected } />;
     },
     renderCell: function(h, { row, column, store, $index }) {
