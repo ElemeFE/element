6699d915dc7ce10a1a324d1c0a674e4a83507a3f
spring <chunyu.ma@brgroup.com>
2021-12-14 15:33:40 +0800

    fix cascader bug when clearing

--- packages/cascader/src/cascader.vue
+++ packages/cascader/src/cascader.vue
@@ -450,6 +450,8 @@ export default {
     handleClear() {
       this.presentText = '';
       this.panel.clearCheckedNodes();
+      this.panel.activePath = [];
+      this.panel.menus = this.panel.menus.length ? this.panel.menus.splice(0, 1) : [];
     },
     handleExpandChange(value) {
       this.$nextTick(this.updatePopper.bind(this));
