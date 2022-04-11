416b42faaf234a7b485c7b748896721bf501431a
Patrick <patik.developer@outlook.com>
2022-04-11 09:35:22 +0200

    i18n: Update Slovenian translation (#21729)

--- src/locale/lang/sl.js
+++ src/locale/lang/sl.js
@@ -107,14 +107,14 @@ export default {
       hasCheckedFormat: '{checked}/{total} izbranih'
     },
     image: {
-      error: 'FAILED' // to be translated
+      error: 'NEUSPELO'
     },
     pageHeader: {
-      title: 'Back' // to be translated
+      title: 'Nazaj'
     },
     popconfirm: {
-      confirmButtonText: 'Yes', // to be translated
-      cancelButtonText: 'No' // to be translated
+      confirmButtonText: 'Da',
+      cancelButtonText: 'Ne'
     },
     empty: {
       description: 'Ni podatkov'
