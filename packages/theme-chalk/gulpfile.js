cc9a7d5ce4d4cfa57f7d0e43b7c5b8949a552c84
bofeng <xinguanhua@gmail.com>
2022-04-12 13:24:48 +0800

    Revert "Chore: replace node-sass with dart-sass (#21019)" (#21791)
    This reverts commit d6dedac2e2cb5f1f171f8ce32a9bbcdf6dab82d4.

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const { series, src, dest } = require('gulp');
-const sass = require('gulp-dart-sass');
+const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 
 function compile() {
   return src('./src/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sass.sync())
     .pipe(autoprefixer({
-      overrideBrowserslist: ['ie > 9', 'last 2 versions'],
+      browsers: ['ie > 9', 'last 2 versions'],
       cascade: false
     }))
     .pipe(cssmin())
