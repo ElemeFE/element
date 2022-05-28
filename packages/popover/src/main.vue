08adf7057f531546fac3a55510b0ec218102d471
kevin <flyo_x@126.com>
2022-05-28 22:19:37 +0800

    popover: fix imported multiple times

--- packages/popover/src/main.vue
+++ packages/popover/src/main.vue
@@ -25,8 +25,7 @@
 </template>
 <script>
 import Popper from 'element-ui/src/utils/vue-popper';
-import { on, off } from 'element-ui/src/utils/dom';
-import { addClass, removeClass } from 'element-ui/src/utils/dom';
+import { on, off, addClass, removeClass } from 'element-ui/src/utils/dom';
 import { generateId } from 'element-ui/src/utils/util';
 
 export default {
