d0f569d13808af04620d3951598178049323d5c1
jcardus <asklocation.net@gmail.com>
2022-06-13 12:23:17 +0100

    Update es.js
    Despejar doesn't make sense in this case.

--- src/locale/lang/es.js
+++ src/locale/lang/es.js
@@ -8,7 +8,7 @@ export default {
       now: 'Ahora',
       today: 'Hoy',
       cancel: 'Cancelar',
-      clear: 'Despejar',
+      clear: 'Limpiar',
       confirm: 'Confirmar',
       selectDate: 'Seleccionar fecha',
       selectTime: 'Seleccionar hora',
@@ -91,7 +91,7 @@ export default {
       emptyText: 'Sin Datos',
       confirmFilter: 'Confirmar',
       resetFilter: 'Reiniciar',
-      clearFilter: 'Despejar',
+      clearFilter: 'Limpiar',
       sumText: 'Suma'
     },
     tree: {
