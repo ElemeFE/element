bb87624b68aac9d0b1996d4dc0926855cc7fa885
Nico Prat <nicooprat@gmail.com>
2022-07-07 11:59:36 +0200

    Fix dropdown ARIA attribute value
    I'm using Axe to automate accessibility issues checking on my app and found this error.
    
    https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-haspopup#values

--- packages/dropdown/src/dropdown.vue
+++ packages/dropdown/src/dropdown.vue
@@ -194,7 +194,7 @@
       },
       initAria() {
         this.dropdownElm.setAttribute('id', this.listId);
-        this.triggerElm.setAttribute('aria-haspopup', 'list');
+        this.triggerElm.setAttribute('aria-haspopup', 'listbox');
         this.triggerElm.setAttribute('aria-controls', this.listId);
 
         if (!this.splitButton) { // 自定义
