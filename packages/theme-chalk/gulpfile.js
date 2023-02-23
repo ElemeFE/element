db03eaade48c764857672c15ec913d6c2e00afab
helianwen <helianwen@innostic.com>
2023-02-23 16:25:08 +0800

    升级node-sass为6.0.1

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const { series, src, dest } = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('node-sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
 
 function compile() {
   return src('./src/*.scss')
-    .pipe(sass.sync())
+    .pipe(sass())
     .pipe(autoprefixer({
       browsers: ['ie > 9', 'last 2 versions'],
       cascade: false
