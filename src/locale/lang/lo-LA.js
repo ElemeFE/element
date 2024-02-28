64359bbcb94840965d330ba99d46c40f59b04989
fengdejin <mydjin@foxmail.com>
2024-02-28 17:01:14 +0800

    i18n: add lo-LA translation Laos translation, the overall content comes from Google translation. It has been verified once with other translation software. There should be no major problems. Please check if there is a translation error AGA before the merger code.

---
+++ src/locale/lang/lo-LA.js
@@ -0,0 +1,126 @@
+'use strict';
+
+exports.__esModule = true;
+exports.default = {
+  el: {
+    colorpicker: {
+      confirm: 'ຢືນຢັນ',
+      clear: 'ເປົ່າ'
+    },
+    datepicker: {
+      now: 'ດຽວນີ້',
+      today: 'ມື້ນີ້',
+      cancel: 'ຍົກເລີກ',
+      clear: 'ເປົ່າ',
+      confirm: 'ຢືນຢັນ',
+      selectDate: 'ເລືອກ ວັນທີ',
+      selectTime: 'ເລືອກ ເວລາ',
+      startDate: 'ວັນ​ທີ່​ເລີ່ມ',
+      startTime: 'ເວລາເລີ່ມຕົ້ນ',
+      endDate: 'ວັນສິ້ນສຸດ',
+      endTime: 'ສິ້ນສຸດເວລາ',
+      prevYear: 'ປີກ່ອນ',
+      nextYear: 'ປີ ຕໍ່ມາ',
+      prevMonth: 'ເດືອນກ່ອນ',
+      nextMonth: 'ເດືອນ ຖັດ ໄປ',
+      year: 'ປີ',
+      month1: 'ມັງກອນ',
+      month2: 'ເດືອນ ກຸມພາ',
+      month3: 'ເດືອນມີນາ',
+      month4: 'ເດືອນເມສາ',
+      month5: 'ເດືອນພຶດສະພາ',
+      month6: 'ມິຖຸນາ',
+      month7: 'ເດືອນກໍລະກົດ',
+      month8: 'ເດືອນ ສິງຫາ',
+      month9: 'ເດືອນ ກັນຍາ',
+      month10: 'ເດືອນ ຕຸລາ',
+      month11: 'ເດືອນພະຈິກ',
+      month12: 'ທັນວາ',
+      // week: '周次',
+      weeks: {
+        sun: 'ວັນ',
+        mon: 'ກ',
+        tue: 'ສອງ',
+        wed: 'ສາມ',
+        thu: 'ສີ່',
+        fri: 'ຫ້າ',
+        sat: 'ຫົກ'
+      },
+      months: {
+        jan: 'ມັງກອນ',
+        feb: 'ເດືອນ ກຸມພາ',
+        mar: 'ເດືອນມີນາ',
+        apr: 'ເດືອນເມສາ',
+        may: 'ເດືອນພຶດສະພາ',
+        jun: 'ມິຖຸນາ',
+        jul: 'ເດືອນກໍລະກົດ',
+        aug: 'ເດືອນ ສິງຫາ',
+        sep: 'ເດືອນ ກັນຍາ',
+        oct: 'ເດືອນ ຕຸລາ',
+        nov: 'ເດືອນພະຈິກ',
+        dec: 'ທັນວາ'
+      }
+    },
+    select: {
+      loading: 'ກຳລັງອອກ...',
+      noMatch: 'ບໍ່ມີຂໍ້ມູນທີ່ກົງກັນ',
+      noData: 'ບໍ່​ມີ​ຂໍ້​ມູນ',
+      placeholder: 'ກະລຸນາເລືອກ'
+    },
+    cascader: {
+      noMatch: 'ບໍ່ມີຂໍ້ມູນທີ່ກົງກັນ',
+      loading: 'ກຳລັງອອກ...',
+      placeholder: 'ກະລຸນາເລືອກ',
+      noData: 'ບໍ່​ມີ​ຂໍ້​ມູນ'
+    },
+    pagination: {
+      goto: 'ໄປທີ່',
+      pagesize: 'ແຖບ/ໜ້າ',
+      total: 'ປົກຕ່າງໆ {total} ກະລຸນາປັດ',
+      pageClassifier: 'ຫມາຍເລກຫນ້າ'
+    },
+    messagebox: {
+      title: 'ຄຳ ແນະ ນຳ',
+      confirm: 'ຢືນຢັນ',
+      cancel: 'ຍົກເລີກ',
+      error: 'ຂໍ້ມູນການປ້ອນຂໍ້ມູນແມ່ນຜິດກົດຫມາຍ!'
+    },
+    upload: {
+      deleteTip: 'ກະລຸນາໃສເພື່ອລຶບ',
+      delete: 'ລຶບ',
+      preview: 'ເບິ່ງຮູບພາບ',
+      continue: 'ສືບຕໍ່ອັບໂຫລດ'
+    },
+    table: {
+      emptyText: 'ບໍ່​ມີ​ຂໍ້​ມູນ',
+      confirmFilter: 'ການ ຄັດເລືອກ',
+      resetFilter: 'ເລີ່ມ​ຕົ້ນ​ໃຫມ່',
+      clearFilter: 'ທັງຫມົດ',
+      sumText: 'ທັງຫມົດ'
+    },
+    tree: {
+      emptyText: 'ບໍ່​ມີ​ຂໍ້​ມູນ'
+    },
+    transfer: {
+      noMatch: 'ບໍ່ມີຂໍ້ມູນທີ່ກົງກັນ',
+      noData: 'ບໍ່​ມີ​ຂໍ້​ມູນ',
+      titles: ['ລາຍຊື່ 1', 'ລາຍຊື່ 2'],
+      filterPlaceholder: 'ໃສ່ເນື້ອຫາຄົ້ນຫາ',
+      noCheckedFormat: 'ປົກຕ່າງໆ {total} ຄໍາສຸດ',
+      hasCheckedFormat: 'ໂຕເລືອກ {checked}/{total} ຄໍາສຸດ'
+    },
+    image: {
+      error: 'ລົ້ມເຫລວໃນການໂຫຼດ'
+    },
+    pageHeader: {
+      title: 'ກັບ ຄືນ'
+    },
+    popconfirm: {
+      confirmButtonText: 'ຢືນຢັນ',
+      cancelButtonText: 'ຍົກເລີກ'
+    },
+    empty: {
+      description: 'ບໍ່​ມີ​ຂໍ້​ມູນ'
+    }
+  }
+};
